ad6fc84041811dd9f410747f64c4e755449b1422
Mel <melcerezo@gmail.com>
2016-10-22 17:12:54 +0800

    renamed .env.example to .env, and created allergies migration

---
+++ database/migrations/2016_10_22_084345_create_allergies_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAllergiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('allergies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('id')->references('id')->on('foodies');
+            $table->foreign('id')->references('id')->on('ingredients');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
