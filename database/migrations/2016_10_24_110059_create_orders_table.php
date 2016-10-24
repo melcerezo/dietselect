cdd7ac70ca49d28dbec83daa5e92f9550d7df24e
Mel <melcerezo@gmail.com>
2016-10-24 19:11:05 +0800

    created orders migration

---
+++ database/migrations/2016_10_24_110059_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreign('id')->references('id')->on('foodies');
+            $table->foreign('id')->references('id')->on('plans');
+            $table->string('orderpaid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
