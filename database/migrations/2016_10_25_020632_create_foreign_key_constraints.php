ca2d29b1dafbc787fb0bd4dbc7eb9f391b9d19cf
Mel <melcerezo@gmail.com>
2016-10-25 10:09:52 +0800

    added foreign key constraints migration

---
+++ database/migrations/2016_10_25_020632_create_foreign_key_constraints.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForeignKeyConstraints extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('allergies', function ($table){
+            $table->foreign('foodies_id')->references('foodies_id')->on('foodies');
+            $table->foreign('ingredients_id')->references('id')->on('ingredients');
+        });
+
+        Schema::table('plans', function ($table){
+            $table->foreign('meals_id')->references('meals_id')->on('meals');
+        });
+
+        Schema::table('meals', function ($table){
+            $table->foreign('chefs_plans_id')->references('chefs_plans_id')->on('chefs_plans');
+        });
+
+        Schema::table('chefs_plans', function ($table){
+            $table->foreign('plans_id')->references('plans_id')->on('plans');
+        });
+
+        Schema::table('orders', function ($table){
+            $table->foreign('plans_id')->references('plans_id')->on('plans');
+            $table->foreign('foodies_id')->references('foodies_id')->on('foodies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
