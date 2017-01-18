affae125bfef5896e44aed309ff9bbdc614f730e
Ronneil Petterson <ronneil.petterson@ffuf.de>
2017-01-19 00:36:57 +0800

    new controller method for GET-ting the meal plans page + formatted code

--- app/Http/Controllers/Chef/MealPlanController.php
+++ app/Http/Controllers/Chef/MealPlanController.php
@@ -1,24 +1,42 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Chef;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MealPlanController extends Controller
 {
-    public function plan(){
+    /**
+     * Check for chef authentication
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('chef.auth');
+    }
+
+    public function getMealPlanPage()
+    {
+        return view('chef.mealplan')->with([
+            'chef' => Auth::guard('chef')->user(),
+        ]);
+    }
+
+    public function setMeal()
+    {
 
     }
 
-    public function setMeal(){
+    public function updateMeal()
+    {
 
     }
 
-    public function updateMeal(){
-
-    }
-
-    public function deleteMeal(){
+    public function deleteMeal()
+    {
 
     }
 }
