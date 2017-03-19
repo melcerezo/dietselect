9954b7efd5c738428ed44455fe0d6fa6ae71e936
melcerezo <melcerezo@gmail.com>
2017-03-19 10:59:10 +0800

    new migrations for customized meals

---
+++ database/migrations/2017_03_19_023917_create_customized_ingredient_meals_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomizedIngredientMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customized_ingredient_meals', function (Blueprint $table) {
+            $table->primary(['meal_id', 'ingredient_id']);
+            $table->integer('meal_id')->unsigned();
+            $table->string('ingredient_id');
+            $table->double('grams');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customized_ingredient_meals');
+    }
+}
---
+++ database/migrations/2017_03_19_025019_create_customized_foreign_keys.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomizedForeignKeys extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('customized_ingredient_meals', function ($table){
+            $table->foreign('meal_id')->references('id')->on('customized_meals');
+            $table->foreign('ingredient_id')->references('NDB_No')->on('ingredients');
+        });
+        Schema::table('customized_meals', function ($table){
+            $table->foreign('foodie_id')->references('id')->on('foodies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
