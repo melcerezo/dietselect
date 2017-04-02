b270e69779d73c69a9bc285a17d2fd0d58e65d3b
Mark Kenneth Santos <santosmken@outlook.ph>
2017-04-02 18:09:08 +0800

    Fixed database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,15 +11,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AllergyTableSeeder::class);
         $this->call(ChefTableSeeder::class);
         $this->call(FoodieTableSeeder::class);
-        $this->call(FoodieAddressTableSeeder::class);
+        $this->call(PlanTableSeeder::class);
+        $this->call(MealTableSeeder::class);
+        $this->call(MealPlanTableSeeder::class);
+//        $this->call(AllergyTableSeeder::class);
+//        $this->call(FoodieAddressTableSeeder::class);
 //        $this->call(FoodiePreferenceTableSeeder::class);
 //        $this->call(MealIngredientTableSeeder::class);
-        $this->call(MealTableSeeder::class);
-        $this->call(PlanTableSeeder::class);
-        $this->call(MealPlanTableSeeder::class);
-
     }
 }
