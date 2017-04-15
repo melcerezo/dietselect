9a2dc23947112a85daa888812150735a48bc23c5
melcerezo <melcerezo@gmail.com>
2017-04-15 16:17:30 +0800

    ratings fixed

--- resources/views/foodie/chefRating.blade.php
+++ resources/views/foodie/chefRating.blade.php
@@ -6,11 +6,9 @@
         <div class="row">
             <div class="card">
                 <div class="card-panel">
-                    @foreach($orders as $order)
-                        @forelse($ratings as $rating)
-                            @if($rating->is_rated == 0)
-                                {{$order->chef->name}}
-                                <form action="{{route('rate.chef', $order->id)}}" method="post">
+                            @if($ratings->is_rated == 0)
+                                {{$orders->chef->name}}
+                                <form action="{{route('rate.chef', $orders->id)}}" method="post">
                                     {{csrf_field()}}
                                     <p>
                                         <input name="rate" type="radio" id="test1" value="1"/>
@@ -43,11 +41,12 @@
 
                                     <input type="submit" value="Submit">
                                 </form>
+
+                                @else
+                                    <div><h3>Thank you for Rating!</h3></div>
                             @endif
-                        @empty
-                            <p>No Orders Yet</p>
-                        @endforelse
-                    @endforeach
+
+
                 </div>
             </div>
         </div>
