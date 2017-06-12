e795004c260023a6b458cec30689dc560e1ad000
melcerezo <melcerezo@gmail.com>
2017-06-12 23:43:21 +0800

    sms

--- app/Http/Controllers/Foodie/Auth/VerifiesSms.php
+++ app/Http/Controllers/Foodie/Auth/VerifiesSms.php
@@ -93,7 +93,7 @@ trait   VerifiesSms
         //code for sms sending ges here
         $mobile='0'.$this->mobile_number();
         $url = 'https://www.itexmo.com/php_api/api.php';
-        $itexmo = array('1' => $mobile, '2' => $code, '3' => 'ST-MARKK578810_4MXKV');
+        $itexmo = array('1' => $mobile, '2' => $code, '3' => 'TR-DIETS656642_GAG39');
         $param = array(
             'http' => array(
                 'header' => "Content-type: application/x-www-form-urlencoded\r\n",
