768447baa0346259b9ef1154f880807bed1a7059
melcerezo <melcerezo@gmail.com>
2017-06-15 01:40:38 +0800

    dashboard cover photo overlay

--- public/js/foodie/foodieDash.js
+++ public/js/foodie/foodieDash.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-    $('.dshPrfCrdCnt').mouseover(function () {
-        $('#addCoverPhotoLink').empty();
-        $('#addCoverPhotoLink').append('+Edit Photo');
-    });
-    $('.dshPrfCrdCnt').mouseleave(function () {
-        $('#addCoverPhotoLink').empty();
-    });
+    // $('.dshPrfCrdCnt').mouseover(function () {
+    //     $('#addCoverPhotoLink').empty();
+    //
+    //     $('#addCoverPhotoLink').append('<span class="orange-text" style="font-size: 40px;">+Edit Photo</span>');
+    // });
+    // $('.dshPrfCrdCnt').mouseleave(function () {
+    //     $('#addCoverPhotoLink').empty();
+    // });
 });
