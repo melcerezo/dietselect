4f3ff6fb42a961b1875c9e5fcabc0cd2aeb9a99d
melcerezo <melcerezo@gmail.com>
2017-06-15 09:22:28 +0800

    order bug

--- resources/views/foodie/showOrder.blade.php
+++ resources/views/foodie/showOrder.blade.php
@@ -94,7 +94,7 @@
                         <div>
                             <span>Delivery Address:</span>
                         </div>
-                        @if($foodieAddress=="")
+                        @if($foodieAddress!=null)
                             <div>
                                 <span>{{$foodieAddress->unit}}</span>
                                 @unless($foodieAddress->bldg=='')
