0541ee928b87c257bb285a5949104b5800137eaf
melcerezo <melcerezo@gmail.com>
2017-06-19 01:23:14 +0800

    testing

--- app/Http/Controllers/Chef/ChefOrderController.php
+++ app/Http/Controllers/Chef/ChefOrderController.php
@@ -78,9 +78,9 @@ class ChefOrderController extends Controller
             for ($i = 0; $i < count($orderMealPlans); $i++) {
                 $orderCustomizedMeals[] = CustomizedMeal::where('meal_id', '=', $orderMealPlans[$i]->chefcustomize->meal_id)->where('order_id', '=', $order->id);
 //                dd($order);
-                for ($j = 0; $j < $orderCustomizedMeals[$i]->customized_ingredient_meal->count(); $j++) {
-                    $ingredientMeals[] = $orderCustomizedMeals[$i]->customized_ingredient_meal[$j];
-                }
+//                for ($j = 0; $j < $orderCustomizedMeals[$i]->customized_ingredient_meal->count(); $j++) {
+//                    $ingredientMeals[] = $orderCustomizedMeals[$i]->customized_ingredient_meal[$j];
+//                }
             }
         }
         for($i=0;$i<count($ingredientMeals);$i++){
