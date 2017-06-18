a44a38113b8d1e7e83c760476840dd8d72deecdc
melcerezo <melcerezo@gmail.com>
2017-06-19 01:39:16 +0800

    testing style

--- resources/views/foodie/mealView.blade.php
+++ resources/views/foodie/mealView.blade.php
@@ -292,13 +292,13 @@
         </div>
         <div class="row">
             <div class="col s12 m6 left plSlMlBtn">
-                <div style="margin-bottom: 20px;">
+                <div class="col s12 m6" style="margin-bottom: 20px;">
                     <button data-target="orderReview" class="btn waves-effect waves-light modal-trigger" style="width: 100%;">Order Plan</button>
                 </div>
-                <div style="margin-bottom: 20px;">
+                <div class="col s12 m6" style="margin-bottom: 20px;">
                     <button id="plSlMlCst" class="btn waves-effect waves-light" style="width: 100%;">Customize Plan</button>
                 </div>
-                <div style="margin-bottom: 20px;">
+                <div class="col s12 m6" style="margin-bottom: 20px;">
                     <button id="plSlMlBck" class="btn waves-effect waves-light" style="color: white; font-weight:100; width: 100%;">Back to Plans</button>
                 </div>
             </div>
