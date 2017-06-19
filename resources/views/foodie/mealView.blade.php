569f6bfdb2ea53751e741ba4ffa5949d05ed234f
melcerezo <melcerezo@gmail.com>
2017-06-20 02:10:25 +0800

    meal view

--- resources/views/foodie/mealView.blade.php
+++ resources/views/foodie/mealView.blade.php
@@ -367,8 +367,8 @@
                     @foreach($mealPhotos->chunk(5) as $chunk)
                         <div class="row">
                             @foreach($chunk as $mealPhoto)
-                                <div class="col s12 m2" style="padding:0 10px 0 10px;">
-                                    <a class="plIndPht" data-id="#plIndSelPht{{ $mealPhoto->id }}"><img class="circle" style="width: 100px;" src="/img/meals/{{ $mealPhoto->image }}"></a>
+                                <div class="col s12 m3">
+                                    <a class="plIndPht" data-id="#plIndSelPht{{ $mealPhoto->id }}"><img class="circle" style="width: 100%;" src="/img/meals/{{ $mealPhoto->image }}"></a>
                                 </div>
                             @endforeach
                         </div>
