3f882b34d968f2b4b9d99acabb18fbd58691199a
melcerezo <melcerezo@gmail.com>
2017-07-09 11:12:20 +0800

    navbar badge

--- resources/views/foodie/layout.blade.php
+++ resources/views/foodie/layout.blade.php
@@ -72,7 +72,7 @@
                                 <span class="nvItTx">
                                     Cart
                                     @if(Cart::count()>0)
-                                        <span class="new badge red">{{Cart::count()}}</span>
+                                        <span class="badge red">{{Cart::count()}}</span>
                                     @endif
                                 </span>
                             </span>
