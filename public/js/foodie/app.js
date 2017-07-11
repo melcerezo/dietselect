d41c2691615e35372dfcac5a4dd72b7a49f22acd
melcerezo <melcerezo@gmail.com>
2017-07-11 09:47:15 +0800

    timeout

--- public/js/foodie/app.js
+++ public/js/foodie/app.js
@@ -1,14 +1,27 @@
 var idleTime = 0;
-
-function timerIncrement() {
-    idleTime = idleTime + 1;
-    if (idleTime > 120) { // 20 minutes
-        console.log('timeout');
-        window.location.href= logoutRoute;
-    }
-}
-
 $(document).ready(function() {
+    function timerIncrement() {
+        idleTime = idleTime + 1;
+        if (idleTime > 120) { // 120 minutes
+            console.log('timeout');
+            window.location.href= logoutRoute;
+        }
+    }
+
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        // console.log(idleTime);
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        console.log('press');
+        idleTime = 0;
+    });
+
+
     $('select').material_select();
     $('.button-collapse').sideNav({
         menuWidth: 300
@@ -140,18 +153,6 @@ $(document).ready(function() {
 
 
 
-    //Increment the idle time counter every minute.
-    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
-
-    //Zero the idle timer on mouse movement.
-    $(this).mousemove(function (e) {
-        // console.log(idleTime);
-        idleTime = 0;
-    });
-    $(this).keypress(function (e) {
-        console.log('press');
-        idleTime = 0;
-    });
 
 
     // $(document).on('click', '#foodieProfile', function () {
