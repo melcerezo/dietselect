f2b4e7484b41ed93adcde487d1c1b66b23efbe2f
melcerezo <melcerezo@gmail.com>
2017-07-23 18:29:32 +0800

    dashboard

--- resources/views/foodie/dashboard.blade.php
+++ resources/views/foodie/dashboard.blade.php
@@ -47,6 +47,7 @@
                         {{--<p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>--}}
                     </div>
                 </div>
+            </div>
                 @if($paidOrderUpcoming!=null)
                     <div class="card">
                             <div class="light-green lighten-1 white-text dshMnClTtl">
