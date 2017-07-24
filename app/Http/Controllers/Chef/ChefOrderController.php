1ac538007280fef2ab2b1db3d3933a7a35ddc7d9
melcerezo <melcerezo@gmail.com>
2017-07-24 21:52:28 +0800

    chef order fix

--- app/Http/Controllers/Chef/ChefOrderController.php
+++ app/Http/Controllers/Chef/ChefOrderController.php
@@ -76,7 +76,7 @@ class ChefOrderController extends Controller
             $orderMealPlans=$orderPlan->customized_meal()->get();
             $orderMealPlansCount = $orderMealPlans->count();
         }
-        dd($orderMealPlans[0]);
+        dd($orderMealPlans[0]->customized_ingredient_meal);
         $orderCustomizedMeals=[];
         $ingredientMeals=[];
         $ingredientMealData=[];
