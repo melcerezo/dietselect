91889cab8e13c89f548b11d828fe323dad884f39
melcerezo <melcerezo@gmail.com>
2017-07-25 18:24:57 +0800

    order single view

--- app/Http/Controllers/Chef/ChefOrderController.php
+++ app/Http/Controllers/Chef/ChefOrderController.php
@@ -78,7 +78,7 @@ class ChefOrderController extends Controller
 //            foreach($orderMealPlans as $orderMealPlan){
 //
 //            }
-        dd($orderMealPlans->customized_ingredient_meal);
+        dd($orderMealPlans[0]->customized_ingredient_meal()->get());
         }
         $orderCustomizedMeals=[];
         $ingredientMeals=[];
