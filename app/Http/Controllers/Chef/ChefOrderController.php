2e078d95b6ef200c6a6ab1694b1ca77618ddc2fc
melcerezo <melcerezo@gmail.com>
2017-07-29 23:20:19 +0800

    meal plan fix

--- app/Http/Controllers/Chef/ChefOrderController.php
+++ app/Http/Controllers/Chef/ChefOrderController.php
@@ -37,7 +37,7 @@ class ChefOrderController extends Controller
 
         $chef = Auth::guard('chef')->user();
 
-        $orders=OrderItem::has('plans')->get();
+        $orders=OrderItem::has('plan')->get();
         dd($orders);
 //        $orders=DB::table('order_items')->join('orders','orders.id','=','order_items.order_id')
 //            ->join('plans','plans.id','=','order_items.plan_id')
