95fb5a487c2ff5217ca7d152621a2c6b686941c1
melcerezo <melcerezo@gmail.com>
2017-07-29 17:07:45 +0800

    meal plan fix

--- resources/views/chef/showSingleOrder.blade.php
+++ resources/views/chef/showSingleOrder.blade.php
@@ -43,8 +43,8 @@
                                 <td class="light-green white-text" style="font-weight: bold;">Monday</td>
                                 <td id="MOBreakfast" data-day="MO" data-meal-type="Breakfast" class="tdCell">
                                     @foreach($mealPlans as $id =>$mealPlan)
-                                        @if($mealPlan->chefcustomize->day == 'MO' && $mealPlan->chefcustomize->meal_type == 'Breakfast')
-                                            {{$mealPlan->chefcustomize->description}}<br>
+                                        @if($mealPlan->mealplans->day == 'MO' && $mealPlan->mealplans->meal_type == 'Breakfast')
+                                            {{$mealPlan->description}}<br>
                                         @endif
                                     @endforeach
                                 </td>
