a246180b0419356dd13e0a24ffa225aed8142559
melcerezo <melcerezo@gmail.com>
2017-08-13 15:30:27 +0800

    modal minimize in view

--- resources/views/foodie/mealView.blade.php
+++ resources/views/foodie/mealView.blade.php
@@ -460,7 +460,7 @@
             </nav>
             <div class="modal-content">
                 <div class="row">
-                    <div class="col s12 m4 offset-m4">
+                    <div class="col s12 m8 offset-m2">
                         <ul class="collection">
                             {{--<li class="collection-item light-green lighten-1 white-text"><span class="collection-header">Order Review</span></li>--}}
                             <li class="collection-item">Chef Name: {{$plan->chef->name}}</li>
@@ -470,7 +470,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col s12 m8 offset-m2">
+                    <div class="col s12 m4 offset-m4">
                         <a href="{{route('cart.add', ['id' => $plan->id,'cust' => 0])}}" class="btn btn-primary waves-effect waves-light" style="font-weight: 100;">Add to Cart</a>
                     </div>
                 </div>
