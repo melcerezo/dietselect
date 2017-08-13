c10c6d8fbb98bc1831ef0ae8494524ca7ca11884
melcerezo <melcerezo@gmail.com>
2017-08-13 17:37:31 +0800

    modal minimize in view

--- app/Http/Controllers/Foodie/FoodieOrderPlanController.php
+++ app/Http/Controllers/Foodie/FoodieOrderPlanController.php
@@ -82,7 +82,7 @@ class FoodieOrderPlanController extends Controller
 
         foreach($orders as $order){
             $dt = new Carbon($order->created_at);
-            $startOfWeek=$dt->startOfWeek()->addDay(7)->format('F d');
+            $startOfWeek=$dt->startOfWeek()->addDay(7)->format('F d, Y');
 //            dd($startOfWeek);
             $orderAddress='';
 
