4b89c9aefec9e82892bd654b16c8e1d921f6e6ca
melcerezo <melcerezo@gmail.com>
2017-08-14 23:19:51 +0800

    cart view

--- resources/views/foodie/cart/index.blade.php
+++ resources/views/foodie/cart/index.blade.php
@@ -95,9 +95,9 @@
                     <div class="col s12 m2">
                         <a id="orderButton" href="{{route('foodie.order')}}">
                             <div class="light-green lighten-1" style="border-radius: 10px;">
-                                <div class="white-text valign-wrapper" style="width: 100%; height: 100px;">
+                                <div class="white-text valign-wrapper" style="width: 100%; height: auto;">
                                     <div class="white-text center-block">
-                                        <i class="fa fa-shopping-bag" style="font-size: 70px;"></i>
+                                        <i class="fa fa-shopping-bag" style="font-size: 30px;"></i>
                                     </div>
                                 </div>
                                 <div class="white-text center">
