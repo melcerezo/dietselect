b949361374e5c479dd68306fd8a96525e36ae366
melcerezo <melcerezo@gmail.com>
2017-08-14 23:45:54 +0800

    cart view

--- resources/views/foodie/showOrder.blade.php
+++ resources/views/foodie/showOrder.blade.php
@@ -75,7 +75,7 @@
                         <div class="col s12 m3">
                             <button data-target="paypalPay" class="modal-trigger btn waves-light waves-effect">
                                     <i class="fa fa-paypal left"></i>
-                                    <span class="white-text">PayPal</span>
+                                    <span class="white-text right">PayPal</span>
                             </button>
                         </div>
                         <div class="col s12 m3">
