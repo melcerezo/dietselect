fcb0155a1da975a6970c7eb3ee548cbd6f18cd18
melcerezo <melcerezo@gmail.com>
2017-08-14 23:17:30 +0800

    cart view

--- resources/views/foodie/cart/index.blade.php
+++ resources/views/foodie/cart/index.blade.php
@@ -79,7 +79,7 @@
         <div class="row">
             <div class="col s12 m6 offset-m6">
                 <div class="row">
-                    <div class="col s12 m4">
+                    <div class="col s12 m4 offset-m6">
                         <ul class="collection">
                            <li class="collection-item light-green lighten-1 white-text">
                                 <span class="collection-header">Total Order:</span>
