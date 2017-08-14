cf2cbc612c803816d67f811bf063515b7001a10b
melcerezo <melcerezo@gmail.com>
2017-08-14 11:48:23 +0800

    template for delivery

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -4,19 +4,19 @@
 
     <div class="container">
         <div class="row">
-            <div class="col s12 m4">
+            <div class="col s12 m2">
                 <div>For Week Of</div>
                 <div>August 14, 2017</div>
             </div>
-            <div class="col s12 m4">
+            <div class="col s12 m2">
                 <div>Total</div>
                 <div>PHP3600.00</div>
             </div>
-            <div class="col s12 m4">
+            <div class="col s12 m6">
                 <div>Address</div>
                 <div>Unit 2636, Tower D, SM Jazz Residences, Nicanor Garcia Corner Jupiter Street, Bel-Air, Makati</div>
             </div>
-            <div class="col s12 m4">
+            <div class="col s12 m2">
                 <div>Status</div>
                 <div>Paid</div>
             </div>
