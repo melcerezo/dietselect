b8cf1adba41e510b405f3f238d4eb0fbe212cb1c
melcerezo <melcerezo@gmail.com>
2017-08-15 19:56:35 +0800

    admin begin

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -344,7 +344,7 @@
             <button class="home" type="button" onclick="window.location='{{ route('welcome') }}'"><i class="fa fa-home"></i></button>
             <button class="fb" type="button"><i class="fa fa-facebook-f"></i></button>
             <button class="email" type="button"><i class="fa fa-envelope fb"></i></button>
-            <button class="partner" type="button" data-target="admin-modal"><i class="fa fa-apple"></i></button>
+            <button class="partner modal-trigger" type="button" data-target="admin-modal"><i class="fa fa-apple"></i></button>
         </div>
     </footer>
     <div id="n-site-preloader" class="valign-wrapper">
