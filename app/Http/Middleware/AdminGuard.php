be6192794a9d092f30be17d090f792130cad81c6
melcerezo <melcerezo@gmail.com>
2017-08-15 19:46:43 +0800

    admin begin

---
+++ app/Http/Middleware/AdminGuard.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AdminGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param string $guard
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $guard = 'admin')
+    {
+        if (Auth::guard($guard)->guest()) {
+            if ($request->ajax() || $request->wantsJson())
+                return response('Unauthorized.', 401);
+            return redirect()->route('/home');//redirect to home;
+        }
+        return $next($request);
+    }
+}
