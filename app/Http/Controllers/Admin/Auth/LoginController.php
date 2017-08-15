b0fe6163c5c3aa0fd6d076f709fdc081ff374bdc
melcerezo <melcerezo@gmail.com>
2017-08-15 22:36:40 +0800

    admin login

--- app/Http/Controllers/Admin/Auth/LoginController.php
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -69,11 +69,18 @@ return $this->sendLockoutResponse($request);
 }
 
 $credentials = $this->credentials($request);
-//dd($this->guard()->attempt($credentials, $request->has('remember')));
-    if (Auth::attempt($credentials)) {
-        // Authentication passed...
-        return $this->sendLoginResponse($request);
-    }
+dd($this->guard()->attempt($credentials, $request->has('remember')));
+
+if (Auth::attempt($credentials)) {
+    // Authentication passed...
+    dd("HELLO");
+    return $this->sendLoginResponse($request);
+}
+
+
+if ($this->guard()->attempt($credentials, $request->has('remember'))) {
+    return $this->sendLoginResponse($request);
+}
 
 // If the login attempt was unsuccessful we will increment the number of attempts
 // to login and redirect the user back to the login form. Of course, when this
