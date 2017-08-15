08fc39681a6057e4b752e51d3237f499fb5c58d3
melcerezo <melcerezo@gmail.com>
2017-08-15 13:26:09 +0800

    chef bank account

--- resources/views/foodie/showOrder.blade.php
+++ resources/views/foodie/showOrder.blade.php
@@ -209,9 +209,9 @@
                 <li class="collection-item light-green lighten-1 white-text"><span class="collection-header">Order Review</span></li>
                 @foreach($orderPlans as $orderPlan)
                     @if($orderPlan->id == $orderItem->plan_id)
-                        @if($orderItem->order_type==0 && $orderPlan->chef->bank_account->bank)
+                        @if($orderItem->order_type==0 && $orderPlan->chef->bank_account->bank != null)
                             <li class="collection-item">{{$orderPlan->chef->bank_account->bank}} Savings Account: {{$orderPlan->chef->bank_account->account}}</li>
-                        @elseif($orderItem->order_type==1  || $orderItem->order_type==2 && $orderPlan->plan->chef->bank_account->bank)
+                        @elseif($orderItem->order_type==1  || $orderItem->order_type==2 && $orderPlan->plan->chef->bank_account->bank!= null)
                             <li class="collection-item">{{$orderPlan->plan->chef->bank_account->bank}} Savings Account: {{$orderPlan->plan->chef->bank_account->account}}</li>
                         @endif
                     @endif
