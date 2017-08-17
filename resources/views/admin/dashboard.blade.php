3857ea37224267a3fe12a6f5b14a5d475af902a4
melcerezo <melcerezo@gmail.com>
2017-08-18 01:05:25 +0800

    admin and commission

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -41,6 +41,19 @@
 
     <div class="container" style="width: 85%;">
         <div class="row">
+            <div class="col s12 m2">
+                <ul class="collection">
+                    <li class="collection-item light-green lighten-1 white-text">
+                        <span class="collection-header">
+                            Admin
+                        </span>
+                    </li>
+                    <li class="collection-item"><a href="#">Commissions</a></li>
+                    <li class="collection-item"><a href="#">Orders</a></li>
+                    <li class="collection-item"><a href="#">Foodies</a></li>
+                    <li class="collection-item"><a href="#">Chefs</a></li>
+                </ul>
+            </div>
             <div class="col s12 m6">
                 <div class="card">
                     <div class="grey lighten-3" style="width: 100%; padding: 10px; border-bottom: solid lightgray 1px;">
@@ -90,20 +103,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col s12 m2">
-                <ul class="collection">
-                    <li class="collection-item light-green lighten-1 white-text">
-                        <span class="collection-header">
-                            Admin
-                        </span>
-                    </li>
-                    <li class="collection-item"><a href="#">Commissions</a></li>
-                    <li class="collection-item"><a href="#">Orders</a></li>
-                    <li class="collection-item"><a href="#">Foodies</a></li>
-                    <li class="collection-item"><a href="#">Chefs</a></li>
-                </ul>
-            </div>
-            <div class="col s12 m5">
+            <div class="col s12 m5 offset-m2">
                <div class="card">
                     <div class="grey lighten-3" style="width: 100%; padding: 10px; border-bottom: solid lightgray 1px;">
                         <div>
