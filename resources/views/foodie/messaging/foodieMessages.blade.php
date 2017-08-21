b5a7c7ed8280ab71871ce076b12ba26232be8bca
melcerezo <melcerezo@gmail.com>
2017-08-21 15:45:18 +0800

    foodie messages

--- resources/views/foodie/messaging/foodieMessages.blade.php
+++ resources/views/foodie/messaging/foodieMessages.blade.php
@@ -257,10 +257,13 @@
     </div>
     <div id="dltCht" class="modal">
         <div class="modal-content">
+            <div>
+                <span>Do you want to delete this chat?</span>
+            </div>
             <form id="foodieMessageReply" action="{{route('foodie.chat.delete',$chatId)}}" method="post">
                 {{csrf_field()}}
                 <div>
-                    <div><input type="submit" value="Submit" class="btn btn-primary"></div>
+                    <div><input type="submit" value="Delete" class="btn btn-primary"></div>
                 </div>
             </form>
         </div>
