0719e74fdcba0e10a64f07971b24b2aa5e450af4
melcerezo <melcerezo@gmail.com>
2017-08-21 20:30:57 +0800

    validate

--- public/js/chef/mealPlannerValidate.js
+++ public/js/chef/mealPlannerValidate.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
             var $errorContainer=ingredIn.parents().eq(1).find($error);
             if($thisVal!=""){
                 var $thisSelect=ingredIn.parents().eq(1).find('select.ingredChefAdd');
-                console.log($thisSelect.find(':selected'));
+                console.log($thisSelect.find(':selected').val());
                 var $valType=$("option:selected",$thisSelect).val().toLowerCase();
                 if($valType=="fruits/fruit juices"){
                     $valType='fruits';
