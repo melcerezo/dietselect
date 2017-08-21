d5dd40fe31e741e90e56c6748004e37745ef3cb9
melcerezo <melcerezo@gmail.com>
2017-08-21 12:15:22 +0800

    dashboard

--- resources/views/chef/dashboard.blade.php
+++ resources/views/chef/dashboard.blade.php
@@ -16,12 +16,18 @@
                             <a id="coverLinkPhoto" href="#addCover" class="modal-trigger">
                                 <span class="fa fa-camera grey-text lighten-3" style="position: absolute; top:12px; left: 0; display: block; font-size: 8em; margin: 0.15em auto; width: 100%; text-align: center;"></span>
                                 <div id="addCoverPhotoLink" style="position: absolute; left:10px; bottom:9px;">
+                                    <div style="padding-top:15%; margin: 0 auto; width: 50%;">
+                                        <span class="orange-text" style="font-size: 40px;">+Add Photo</span>
+                                    </div>
                                 </div>
                             </a>
                         @else
                             <a href="#addCover" class="modal-trigger">
                                 <img class="dshPrfCrdImgBan" src="/img/{{ $chef->cover }}">
                                 <div id="addCoverPhotoLink" style="position: absolute; left:10px; bottom:9px;">
+                                    <div style="padding-top:15%; margin: 0 auto; width: 50%;">
+                                        <span class="orange-text" style="font-size: 40px;">+Add Photo</span>
+                                    </div>
                                 </div>
                             </a>
                         @endif
