f480d5b0c1c466d3077d5f76188a648ad2ca7cc5
melcerezo <melcerezo@gmail.com>
2017-08-21 14:33:07 +0800

    rating

--- resources/views/foodie/chefRating.blade.php
+++ resources/views/foodie/chefRating.blade.php
@@ -15,7 +15,7 @@
             </nav>
         </div>
         @if(count($ordersRatingChef)>0)
-            @foreach($ordersRatingChef as $order)
+            @foreach($ordersRatingChef as $key=>$order)
                 <div class="row">
                     <div class="card">
                         <nav class="light-green lighten-1 white-text">
@@ -31,25 +31,25 @@
                             <span>Please Rate the Plan!</span>
                             <form action="{{route('rate.chef', $order['id'])}}" method="post">
                                 {{csrf_field()}}
-                                <input name="rate" type="radio" id="test1" value="1" checked/>
+                                <input name="rate{{$key}}" type="radio" id="test1" value="1" checked/>
                                 <label for="test1">1</label>
 
-                                <input name="rate" type="radio" id="test2" value="2"/>
+                                <input name="rate{{$key}}" type="radio" id="test2" value="2"/>
                                 <label for="test2">2</label>
 
-                                <input class="with-gap" name="rate" type="radio" id="test3" value="3"/>
+                                <input class="with-gap" name="rate{{$key}}" type="radio" id="test3" value="3"/>
                                 <label for="test3">3</label>
 
-                                <input class="with-gap" name="rate" type="radio" id="test4" value="4"/>
+                                <input class="with-gap" name="rate{{$key}}" type="radio" id="test4" value="4"/>
                                 <label for="test4">4</label>
 
-                                <input class="with-gap" name="rate" type="radio" id="test5" value="5"/>
+                                <input class="with-gap" name="rate{{$key}}" type="radio" id="test5" value="5"/>
                                 <label for="test5">5</label>
 
                                 <div class="row">
                                     <div class="input-field col s12">
                                 <textarea id="textarea1" class="materialize-textarea" data-length="120"
-                                          name="feedback"></textarea>
+                                          name="feedback{{$key}}"></textarea>
                                         <label for="textarea1">Comment:</label>
                                     </div>
                                 </div>
