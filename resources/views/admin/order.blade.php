00bf830ce624541eb513feda50fe63c3d54bd4e9
melcerezo <melcerezo@gmail.com>
2017-08-23 03:14:44 +0800

    order admin

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -107,7 +107,7 @@
                                                 <div class="col s12 m3">
                                                     <img src="/img/{{$orderItemItem['planPic']}}" class="img-responsive" style="max-width:150px;"/>
                                                 </div>
-                                                <div class="col s12 m4" style="font-size: 20px;">
+                                                <div class="col s12 m6" style="font-size: 20px;">
                                                     <div>{{$orderItemItem['plan']}}</div>
                                                     <div>Made By: {{$orderItemItem['chef']}}</div>
                                                     <div>Type:  {{$orderItemItem['type']}}</div>
