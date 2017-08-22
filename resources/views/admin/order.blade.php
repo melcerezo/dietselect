261add6145c140f777ddf1915ee56b3e3426cc68
melcerezo <melcerezo@gmail.com>
2017-08-23 02:47:29 +0800

    order admin

--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -91,32 +91,36 @@
                         </div>
                     </div>
                     <div class="card-content">
-                        <div class="grey lighten-3" style="width: 100%; padding: 10px; border-bottom: solid lightgray 1px;">
-                            <div>
-                            <span>
-                                Order Items
-                            </span>
+                        <div class="row">
+                            <div class="col s12 m9">
+                                <div class="grey lighten-3" style="width: 100%; padding: 10px; border-bottom: solid lightgray 1px;">
+                                    <div>
+                                        <span>
+                                            Order Items
+                                        </span>
+                                    </div>
+                                </div>
+                                @foreach($orderItemArray as $orderItemItem)
+                                    @if($orderItemItem['order_id']==$order->id)
+                                        <div class="row">
+                                            <div class="col s12 m3">
+                                                <img src="/img/{{$orderItemItem['planPic']}}" class="img-responsive" style="max-width:150px;"/>
+                                            </div>
+                                            <div class="col s12 m4" style="font-size: 20px;">
+                                                <div>{{$orderItemItem['plan']}}</div>
+                                                <div>Made By: {{$orderItemItem['chef']}}</div>
+                                                <div>Type:  {{$orderItemItem['type']}}</div>
+                                                <div>Quantity: {{$orderItemItem['quantity']}}</div>
+                                                <div>Amount: {{$orderItemItem['price']}}</div>
+                                            </div>
+                                            <div class="col s12 offset-m1 m2">
+                                                {{--<a href="{{route('admin.plan', $order->id)}}" class="btn btn-primary waves-effect waves-light" style="font-weight: 100; width:100%;">Details</a>--}}
+                                            </div>
+                                        </div>
+                                    @endif
+                                @endforeach
                             </div>
                         </div>
-                            @foreach($orderItemArray as $orderItemItem)
-                                @if($orderItemItem['order_id']==$order->id)
-                                    <div class="row">
-                                        <div class="col s12 m3">
-                                            <img src="/img/{{$orderItemItem['planPic']}}" class="img-responsive" style="max-width:150px;"/>
-                                        </div>
-                                        <div class="col s12 m4" style="font-size: 20px;">
-                                            <div>{{$orderItemItem['plan']}}</div>
-                                            <div>Made By: {{$orderItemItem['chef']}}</div>
-                                            <div>Type:  {{$orderItemItem['type']}}</div>
-                                            <div>Quantity: {{$orderItemItem['quantity']}}</div>
-                                            <div>Amount: {{$orderItemItem['price']}}</div>
-                                        </div>
-                                        <div class="col s12 offset-m1 m2">
-                                            {{--<a href="{{route('admin.plan', $order->id)}}" class="btn btn-primary waves-effect waves-light" style="font-weight: 100; width:100%;">Details</a>--}}
-                                        </div>
-                                    </div>
-                                @endif
-                            @endforeach
                         <div class="row">
                             <div class="col s12 m4">
                                 <ul class="collection">
