05d1bebd50bc1abeddbc6b60bd2969106a965d9a
melcerezo <melcerezo@gmail.com>
2017-08-23 23:54:09 +0800

    all order view

--- resources/views/foodie/viewAllOrders.blade.php
+++ resources/views/foodie/viewAllOrders.blade.php
@@ -74,7 +74,7 @@
                             @foreach($orderItemArray as $orderItemItem)
                                 @if($orderItemItem['order_id']==$order->id)
                             <div class="row">
-                                <div class="col s12 m2">
+                                <div class="col s12 m3">
                                     <img src="/img/{{$orderItemItem['planPic']}}" class="img-responsive" style="max-width:150px;"/>
                                 </div>
                                 <div class="col s12 m4" style="font-size: 20px;">
