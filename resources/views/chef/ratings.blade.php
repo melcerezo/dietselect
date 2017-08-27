0a0c3df2232c1a586aac7469f8c0184cf94342a3
melcerezo <melcerezo@gmail.com>
2017-08-27 18:31:03 +0800

    ratings

--- resources/views/chef/ratings.blade.php
+++ resources/views/chef/ratings.blade.php
@@ -20,7 +20,8 @@
                 @if($rating->is_rated==1)
                 <div class="card">
                     <div class="card-panel">
-                        <h4>Rated By: {{$rating->foodie->first_name.' '.$rating->foodie->last_name}}</h4>
+                        <img src="/img/{{ $rating->foodie->avatar }}">
+                        <h4>{{$rating->foodie->first_name.' '.$rating->foodie->last_name}}</h4>
                         @if($rating->rating == 5)
                             @for($i=0; $i<5; $i++)
                                 <span><i class="fa fa-star" style="color: yellow"></i></span>
