9080ca686d2e9ed9d31a9e26e18557dbd4d4a499
melcerezo <melcerezo@gmail.com>
2017-08-28 22:55:38 +0800

    chefRating

--- resources/views/foodie/chefRating.blade.php
+++ resources/views/foodie/chefRating.blade.php
@@ -1,5 +1,34 @@
 @extends('foodie.layout')
-
+@section('page_head')
+    <style>
+        .starRating:not(old) > input{
+            margin-right : -100%;
+            opacity      : 0;
+        }
+        .starRating:not(old) > label{
+            display         : block;
+            float           : right;
+            position        : relative;
+            background      : url('/img/star-off.svg');
+            background-size : contain;
+        }
+        .starRating:not(old) > label:before{
+            content         : '';
+            display         : block;
+            width           : 1.5em;
+            height          : 1.5em;
+            background      : url('/img/star-on.svg');
+            background-size : contain;
+            opacity         : 0;
+            transition      : opacity 0.2s linear;
+        }
+        .starRating:not(old) > label:hover:before,
+        .starRating:not(old) > label:hover ~ label:before,
+        .starRating:not(:hover) > :checked ~ label:before{
+            opacity : 1;
+        }
+    </style>
+@endsection
 @section('page_content')
 
     <div class="container" style="margin-top: 10px;">
@@ -31,21 +60,22 @@
                             <span>Please Rate the Plan!</span>
                             <form action="{{route('rate.chef', [$order['id'],$key])}}" method="post">
                                 {{csrf_field()}}
-                                <input name="rate{{$key}}" type="radio" id="test1{{$key}}" value="1" checked/>
-                                <label for="test1{{$key}}">1</label>
+                                <span class="starRating">
+                                    <input name="rate{{$key}}" type="radio" id="test1{{$key}}" value="1" checked/>
+                                    <label for="test1{{$key}}">1</label>
 
-                                <input name="rate{{$key}}" type="radio" id="test2{{$key}}" value="2"/>
-                                <label for="test2{{$key}}">2</label>
+                                    <input name="rate{{$key}}" type="radio" id="test2{{$key}}" value="2"/>
+                                    <label for="test2{{$key}}">2</label>
 
-                                <input class="with-gap" name="rate{{$key}}" type="radio" id="test3{{$key}}" value="3"/>
-                                <label for="test3{{$key}}">3</label>
+                                    <input class="with-gap" name="rate{{$key}}" type="radio" id="test3{{$key}}" value="3"/>
+                                    <label for="test3{{$key}}">3</label>
 
-                                <input class="with-gap" name="rate{{$key}}" type="radio" id="test4{{$key}}" value="4"/>
-                                <label for="test4{{$key}}">4</label>
+                                    <input class="with-gap" name="rate{{$key}}" type="radio" id="test4{{$key}}" value="4"/>
+                                    <label for="test4{{$key}}">4</label>
 
-                                <input class="with-gap" name="rate{{$key}}" type="radio" id="test5{{$key}}" value="5"/>
-                                <label for="test5{{$key}}">5</label>
-
+                                    <input class="with-gap" name="rate{{$key}}" type="radio" id="test5{{$key}}" value="5"/>
+                                    <label for="test5{{$key}}">5</label>
+                                </span>
                                 <div class="row">
                                     <div class="input-field col s12">
                                 <textarea id="textarea1{{$key}}" class="materialize-textarea" data-length="120"
