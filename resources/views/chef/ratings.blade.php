4333baab4d6305f779cb76463d1a9e1f352a4dfa
melcerezo <melcerezo@gmail.com>
2017-08-29 22:56:27 +0800

    chef rating

--- resources/views/chef/ratings.blade.php
+++ resources/views/chef/ratings.blade.php
@@ -16,34 +16,45 @@
             </nav>
         </div>
         <div class="row">
-            @forelse($ratings as $rating)
+            @forelse($ratings as $key=>$rating)
                 @if($rating->is_rated==1)
                 <div class="card">
                     <div class="card-panel">
                         <div>
                             <h4><img class="circle" style="width: 60px;" src="/img/{{ $rating->foodie->avatar }}"> {{$rating->foodie->first_name.' '.$rating->foodie->last_name}}</h4>
                         </div>
-                        @if($rating->rating == 5)
-                            @for($i=0; $i<5; $i++)
-                                <span><i class="fa fa-star" style="color: gold"></i></span>
-                            @endfor
-                        @elseif($rating->rating == 4)
-                            @for($i=0; $i<4; $i++)
-                                <span><i class="fa fa-star" style="color: gold"></i></span>
-                            @endfor
-                        @elseif($rating->rating == 3)
-                            @for($i=0; $i<3; $i++)
-                                <span><i class="fa fa-star" style="color: gold"></i></span>
-                            @endfor
-                        @elseif($rating->rating == 2)
-                            @for($i=0; $i<2; $i++)
-                                <span><i class="fa fa-star" style="color: gold"></i></span>
-                            @endfor
-                        @elseif($rating->rating == 1)
-                            @for($i=0; $i<1; $i++)
-                                <span><i class="fa fa-star" style="color: gold"></i></span>
-                            @endfor
-                        @endif
+                        <div id="rateYo{{$key}}">
+                        </div>
+                        <script>
+                            $(function () {
+                                $("#rateYo{{$key}}").rateYo({
+                                    rating: '{{$rating->rating}}',
+                                    fullStar: true,
+                                    readOnly: true
+                                });
+                            });
+                        </script>
+                        {{--@if($rating->rating == 5)--}}
+                            {{--@for($i=0; $i<5; $i++)--}}
+                                {{--<span><i class="fa fa-star" style="color: gold"></i></span>--}}
+                            {{--@endfor--}}
+                        {{--@elseif($rating->rating == 4)--}}
+                            {{--@for($i=0; $i<4; $i++)--}}
+                                {{--<span><i class="fa fa-star" style="color: gold"></i></span>--}}
+                            {{--@endfor--}}
+                        {{--@elseif($rating->rating == 3)--}}
+                            {{--@for($i=0; $i<3; $i++)--}}
+                                {{--<span><i class="fa fa-star" style="color: gold"></i></span>--}}
+                            {{--@endfor--}}
+                        {{--@elseif($rating->rating == 2)--}}
+                            {{--@for($i=0; $i<2; $i++)--}}
+                                {{--<span><i class="fa fa-star" style="color: gold"></i></span>--}}
+                            {{--@endfor--}}
+                        {{--@elseif($rating->rating == 1)--}}
+                            {{--@for($i=0; $i<1; $i++)--}}
+                                {{--<span><i class="fa fa-star" style="color: gold"></i></span>--}}
+                            {{--@endfor--}}
+                        {{--@endif--}}
                         @if($rating->feedback!=null)
                             <p>Comment: {{$rating->feedback}}</p>
                         @endif
