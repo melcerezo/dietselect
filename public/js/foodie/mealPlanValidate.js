80c2ed597125f5808d949f162a0901fc19f81522
melcerezo <melcerezo@gmail.com>
2017-09-03 12:25:19 +0800

    validate

--- public/js/foodie/mealPlanValidate.js
+++ public/js/foodie/mealPlanValidate.js
@@ -64,7 +64,7 @@ $(document).ready(function () {
         $("form.editMeal :input").each(function () {
             var type = $(this).getType();
             var id = $(this).attr('id');
-            console.log(id);
+            console.log($(this).attr('id'));
             if (type == 'text' || type == 'select' || type=='number') {
                 disable = (orig[id].value == $(this).val());
             } else if (type == 'radio') {
