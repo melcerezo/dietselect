3ace74bd9a33790e58d32b5a621aa6941a359467
melcerezo <melcerezo@gmail.com>
2017-09-03 21:57:00 +0800

    meal plan controller

--- app/Http/Controllers/Foodie/FoodieMealPlanController.php
+++ app/Http/Controllers/Foodie/FoodieMealPlanController.php
@@ -687,9 +687,7 @@ class FoodieMealPlanController extends Controller
 
 
         foreach($origMeal->customized_ingredient_meal()->get() as $i=>$item) {
-            if($ingredId[$i]->NDB_No!=$item->ingredient_id && $request['grams'][$arrayKeys[$i]]==$item->grams
-                || $ingredId[$i]->NDB_No==$item->ingredient_id && $request['grams'][$arrayKeys[$i]]!=$item->grams
-                || $ingredId[$i]->NDB_No!=$item->ingredient_id && $request['grams'][$arrayKeys[$i]]!=$item->grams){
+            if($ingredId[$i]->NDB_No!=$item->ingredient_id || $request['grams'][$arrayKeys[$i]]!=$item->grams){
                 DB::table('customized_ingredient_meals')->where('id','=',$customIngred[$i]->id)->where('ingredient_id','=',$prevIngreds[$i]->ingredient_id)->update(
                     ['meal_id' => $customize->id,'is_customized'=>1, 'ingredient_id' => $ingredId[$i]->NDB_No, 'grams' => $request['grams'][$arrayKeys[$i]]]
                 );
