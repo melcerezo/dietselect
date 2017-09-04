b45eead42815e0011dbd6d9781dbb34f57fca9c9
melcerezo <melcerezo@gmail.com>
2017-09-04 12:12:52 +0800

    email fix

--- app/Http/Controllers/Foodie/FoodieOrderPlanController.php
+++ app/Http/Controllers/Foodie/FoodieOrderPlanController.php
@@ -417,7 +417,7 @@ class FoodieOrderPlanController extends Controller
                 'date'=>$thisSaturday
             ];
         }
-        dd($mailHTML);
+//        dd($mailHTML);
 
 
         $price = Cart::total();
