7f919dcc4ed0e3d2ab553c2770e76b92db653bbf
melcerezo <melcerezo@gmail.com>
2017-09-06 01:10:25 +0800

    cust

--- resources/views/foodie/simpleCustomize.blade.php
+++ resources/views/foodie/simpleCustomize.blade.php
@@ -571,7 +571,7 @@
         </div>
     </div>
 
-    @foreach($simpleCustomMeals as $simpleCustomMeal)
+    @foreach($simpleCustomMeals as $i=>$simpleCustomMeal)
         <div id="editMeal-{{$i}}" class="editFoodieCustModal modal">
             <nav class="light-green lighten-1 white-text">
                 <div class="left col s12 m5 l5">
