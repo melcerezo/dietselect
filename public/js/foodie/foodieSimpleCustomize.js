729d85ce9dccddc2186dceec27d61c94529c3407
melcerezo <melcerezo@gmail.com>
2017-09-06 17:55:18 +0800

    simp cust

--- public/js/foodie/foodieSimpleCustomize.js
+++ public/js/foodie/foodieSimpleCustomize.js
@@ -98,7 +98,8 @@ $(document).ready(function () {
                        '<label for="eggs'+id+'">No Eggs</label><br/>' +
                        '</div>');
                    // console.log(valData[j].ingredient.indexOf("Egg"));
-               }else if(valData[j].ingredient_group=='~0100~'
+               }
+               if(valData[j].ingredient_group=='~0100~'
                    && valData[j].ingredient.indexOf("Egg")<0
                    || valData[j].ingredient.indexOf("milk")>=0
                    || valData[j].ingredient.indexOf("Milk")>=0
@@ -111,7 +112,12 @@ $(document).ready(function () {
                        '</div>');
                    // console.log(valData[j].ingredient.indexOf("Egg"));
                }
-
+                console.log(valData[j].ingredient_group=='~0100~'
+                    && valData[j].ingredient.indexOf("Egg")<0
+                    || valData[j].ingredient.indexOf("milk")>=0
+                    || valData[j].ingredient.indexOf("Milk")>=0
+                    || valData[j].ingredient.indexOf("MILK")>=0
+                    && dairyType.find('#dairy'+id).length==0);
                if(valData[j].ingredient.indexOf("Peanut")>=0 || valData[j].ingredient.indexOf("peanut")>=0
                    || valData[j].ingredient_group=='~1200~' && !produceType.find('#nut'+id).length){
                    produceType.append('<div id="nut'+id+'">' +
