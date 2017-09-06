d363da905109c7d903f0686790350e8e22471a59
melcerezo <melcerezo@gmail.com>
2017-09-06 17:05:44 +0800

    cust

--- public/js/foodie/foodieSimpleCustomize.js
+++ public/js/foodie/foodieSimpleCustomize.js
@@ -115,6 +115,9 @@ $(document).ready(function () {
                        '<label for="dairy'+id+'">No Nut/Nut Products</label><br/>' +
                        '</div>');
                }
+               console.log(valData[j].ingredient.indexOf("Peanut"));
+               console.log(valData[j].ingredient_group=='~1600~');
+
             }
 
 
