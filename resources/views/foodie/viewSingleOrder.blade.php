46cc9fe6a9e4d6a138a4095a112baa10cd8762e3
melcerezo <melcerezo@gmail.com>
2017-09-07 21:25:33 +0800

    order single

--- resources/views/foodie/viewSingleOrder.blade.php
+++ resources/views/foodie/viewSingleOrder.blade.php
@@ -1435,7 +1435,7 @@
                 @if($orderItem->order->is_paid==0)
                     <a href="{{route('order.show',$orderItem->order->id)}}" class="btn waves-effect waves-light">Back to Order</a>
                 @elseif($orderItem->order->is_paid==1)
-                    <a href="{{route('order.show',$orderItem->order->id)}}" class="btn waves-effect waves-light">Back to Order</a>
+                    <a href="{{route('foodie.order.view',$orderItem->order->id)}}" class="btn waves-effect waves-light">Order History</a>
                 @endif
                 @if($orderItem->order_type==2)
                     @if($orderPlan->simple_custom_plan_detail->count())
