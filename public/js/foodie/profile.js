80ddaf483d1d72545fa65277dea2b770f2c8a908
melcerezo <melcerezo@gmail.com>
2017-09-08 00:59:32 +0800

    stat

--- public/js/foodie/profile.js
+++ public/js/foodie/profile.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
         $('#basic-profile-container,#addresses-container,#allergies-container').hide();
     });
 
+    console.log(Date.now());
 
     $('#birthday').pickadate({
         // Buttons
