394c41b301a427f5b313ebaab6e4f5cbc36e61ed
melcerezo <melcerezo@gmail.com>
2017-09-09 02:01:20 +0800

    test

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -147,6 +147,71 @@
                         </div>
                     </div>
                 </div>
+                <div class="row">
+                    <div class="col s12 m4">
+                        <div class="card buyCard">
+                            <div class="card-image">
+                                <img src="img/bg.jpg">
+                                <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
+                                    PHP 1,800.00
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div>
+                                    Diet 1
+                                </div>
+                                <div>
+                                    Description about this diet.
+                                </div>
+                                <div class="buyBtn center-align">
+                                    <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card buyCard">
+                            <div class="card-image">
+                                <img src="img/bg.jpg">
+                                <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
+                                    PHP 1,800.00
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div>
+                                    Diet 2
+                                </div>
+                                <div>
+                                    Description about this diet.
+                                </div>
+                                <div class="buyBtn center-align">
+                                    <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m4">
+                        <div class="card buyCard">
+                            <div class="card-image">
+                                <img src="img/bg.jpg">
+                                <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
+                                    PHP 1,800.00
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div>
+                                    Diet 3
+                                </div>
+                                <div>
+                                    Description about this diet.
+                                </div>
+                                <div class="buyBtn center-align">
+                                    <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
