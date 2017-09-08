e8ef5ddaac1d89f44a7621cfb49a60db656d4670
melcerezo <melcerezo@gmail.com>
2017-09-09 01:02:03 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -32,7 +32,7 @@
                 </div>
             </div>
         </div>
-        <div class="row products">
+        <div class="row">
             <div class="col s12 m3">
                <div>
                     <span>By Chef</span>
@@ -43,65 +43,70 @@
                    <li>Gourmet Kusina</li>
                </ul>
             </div>
-            <div class="col s12 m3">
-                <div class="card buyCard">
-                    <div class="card-image">
-                        <img src="img/bg.jpg">
-                        <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
-                            PHP 1,800.00
+
+            <div class="col s9">
+                <div class="row">
+                    <div class="col s12 m3">
+                        <div class="card buyCard">
+                            <div class="card-image">
+                                <img src="img/bg.jpg">
+                                <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
+                                    PHP 1,800.00
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div>
+                                    Diet 1
+                                </div>
+                                <div>
+                                    Description about this diet.
+                                </div>
+                                <div class="buyBtn center-align">
+                                    <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="card-content">
-                        <div>
-                            Diet 1
-                        </div>
-                        <div>
-                            Description about this diet.
-                        </div>
-                        <div class="buyBtn center-align">
-                            <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col s12 m3">
-                <div class="card buyCard">
-                    <div class="card-image">
-                        <img src="img/bg.jpg">
-                        <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
-                            PHP 1,800.00
-                        </div>
-                    </div>
-                    <div class="card-content">
-                        <div>
-                            Diet 2
-                        </div>
-                        <div>
-                            Description about this diet.
-                        </div>
-                        <div class="buyBtn center-align">
-                            <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                    <div class="col s12 m3">
+                        <div class="card buyCard">
+                            <div class="card-image">
+                                <img src="img/bg.jpg">
+                                <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
+                                    PHP 1,800.00
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div>
+                                    Diet 2
+                                </div>
+                                <div>
+                                    Description about this diet.
+                                </div>
+                                <div class="buyBtn center-align">
+                                    <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col s12 m3">
-                <div class="card buyCard">
-                    <div class="card-image">
-                        <img src="img/bg.jpg">
-                        <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
-                            PHP 1,800.00
-                        </div>
-                    </div>
-                    <div class="card-content">
-                        <div>
-                            Diet 3
-                        </div>
-                        <div>
-                            Description about this diet.
-                        </div>
-                        <div class="buyBtn center-align">
-                            <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                    <div class="col s12 m3">
+                        <div class="card buyCard">
+                            <div class="card-image">
+                                <img src="img/bg.jpg">
+                                <div class="orange white-text" style="position: absolute; left: 5px; top: 5px;">
+                                    PHP 1,800.00
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <div>
+                                    Diet 3
+                                </div>
+                                <div>
+                                    Description about this diet.
+                                </div>
+                                <div class="buyBtn center-align">
+                                    <button class="orange btn btn-primary waves-effect waves-light">Buy Now</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
