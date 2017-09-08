faefdac90173944d573e8f5cf257d4a20c3a4af3
melcerezo <melcerezo@gmail.com>
2017-09-08 22:05:33 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -2,11 +2,11 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col s12 m4 light-green">
+            <div class="col s12 m4 light-green" style="height: 200px;">
             </div>
-            <div class="col s12 m4 light-green">
+            <div class="col s12 m4 light-green" style="height: 200px;">
             </div>
-            <div class="col s12 m4 light-green">
+            <div class="col s12 m4 light-green" style="height: 200px;">
             </div>
         </div>
         <div class="row">
