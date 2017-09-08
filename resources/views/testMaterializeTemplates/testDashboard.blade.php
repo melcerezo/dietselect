ead2f1ee780140400ead6fe4e72e91e28dc80283
melcerezo <melcerezo@gmail.com>
2017-09-08 22:44:54 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -28,19 +28,51 @@
         </div>
         <div class="row">
             <div class="col s12 m3">
-                <div class="light-green" style="height: 200px;">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/bg.jpg">
+                        <span class="card-title">Card Title</span>
+                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                    </div>
                 </div>
             </div>
             <div class="col s12 m3">
-                <div class="light-green" style="height: 200px;">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/bg.jpg">
+                        <span class="card-title">Card Title</span>
+                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                    </div>
                 </div>
             </div>
             <div class="col s12 m3">
-                <div class="light-green" style="height: 200px;">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/bg.jpg">
+                        <span class="card-title">Card Title</span>
+                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                    </div>
                 </div>
             </div>
             <div class="col s12 m3">
-                <div class="light-green" style="height: 200px;">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/bg.jpg">
+                        <span class="card-title">Card Title</span>
+                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
+                    </div>
                 </div>
             </div>
         </div>
