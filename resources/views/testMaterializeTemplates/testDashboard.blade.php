357e30c2d8e059f951bcdf8460577da183b747ac
melcerezo <melcerezo@gmail.com>
2017-09-08 22:26:28 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -27,15 +27,16 @@
             </div>
         </div>
         <div class="row">
-            <div class="col s12 m4">
+
+            <div class="col s12 m3 offset-m3">
                 <div class="light-green" style="height: 200px;">
                 </div>
             </div>
-            <div class="col s12 m4">
+            <div class="col s12 m3 offset-m3">
                 <div class="light-green" style="height: 200px;">
                 </div>
             </div>
-            <div class="col s12 m4">
+            <div class="col s12 m3 offset-m3">
                 <div class="light-green" style="height: 200px;">
                 </div>
             </div>
