38479eaa0f4b553b7f7b104095848c8296de52e4
melcerezo <melcerezo@gmail.com>
2017-09-08 21:43:15 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -2,28 +2,9 @@
 @section('content')
 
 <header>
-    <div>
-        <ul class="right">
-            <li>Messages</li>
-            <li>Notifications</li>
-            <li>FoodieName</li>
-        </ul>
-    </div>
-    <div>
-        <nav>
-            <div style="display: inline;">
-                <span>Brand</span>
-            </div>
-           <ul>
-               <li>Cart</li>
-           </ul>
-        </nav>
-    </div>
-    <div>
-        <ul>
-            <li>Plans</li>
-        </ul>
-    </div>
+   <nav>Bar</nav>
+   <nav>Bar</nav>
+   <nav>Bar</nav>
 </header>
 
 @endsection
