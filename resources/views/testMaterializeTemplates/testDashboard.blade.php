7e1f53e2d4bc7c4780b5b9181e3c737d0c9a6518
melcerezo <melcerezo@gmail.com>
2017-09-08 21:47:48 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -2,9 +2,23 @@
 @section('content')
 
 <header>
-   <nav style="height: 25px; line-height: 25px;">Bar</nav>
-   <nav>Bar</nav>
-   <nav style="height: 25px; line-height: 25px;">Bar</nav>
+   <nav style="height: 25px; line-height: 25px;">
+       <ul class="right">
+           <li>Messages</li>
+           <li>Notification</li>
+           <li>foodieName</li>
+       </ul>
+   </nav>
+   <nav>
+       <div>
+           <span>Diet Select</span>
+       </div>
+   </nav>
+   <nav style="height: 25px; line-height: 25px;">
+       <ul class="left">
+           <li>Plans</li>
+       </ul>
+   </nav>
 </header>
 
 @endsection
