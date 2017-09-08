4adca0cf318146e82029ea7169e66c495d31c7ae
melcerezo <melcerezo@gmail.com>
2017-09-08 21:52:47 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -2,26 +2,30 @@
 @section('content')
 
 <header>
-   <nav style="height: 25px; line-height: 25px;">
-       <ul class="right">
-           <li>Messages</li>
-           <li>Notification</li>
-           <li>foodieName</li>
-       </ul>
-   </nav>
-   <nav>
-       <div>
-           <span>Diet Select</span>
-       </div>
-       <ul>
-          <li>Cart</li>
-       </ul>
-   </nav>
-   <nav style="height: 25px; line-height: 25px;">
-       <ul class="left">
-           <li>Plans</li>
-       </ul>
-   </nav>
+    <nav class="nav-extended">
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">Logo</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+            <ul class="side-nav" id="mobile-demo">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+        </div>
+        <div class="nav-content">
+            <ul class="tabs tabs-transparent">
+                <li class="tab"><a href="#test1">Test 1</a></li>
+                <li class="tab"><a class="active" href="#test2">Test 2</a></li>
+                <li class="tab disabled"><a href="#test3">Disabled Tab</a></li>
+                <li class="tab"><a href="#test4">Test 4</a></li>
+            </ul>
+        </div>
+    </nav>
 </header>
 
 @endsection
