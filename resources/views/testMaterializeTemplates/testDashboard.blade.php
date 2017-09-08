dfa9196933833835eefa1fafc39b69c8ff3eecc8
melcerezo <melcerezo@gmail.com>
2017-09-09 01:17:54 +0800

    test

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -9,11 +9,7 @@
     <script>
         $(document).ready(function () {
             $('div.buyCard').hover(function () {
-                if($(this).find('.card-content').find('.buyBtn').css('visibility')=='hidden'){
-                    $(this).find('.card-content').find('.buyBtn').css('visibility','visible');
-                }else{
-                    $(this).find('.card-content').find('.buyBtn').css('visibility','hidden');
-                }
+                $(this).find('.card-content').find('.buyBtn').toggle();
             });
         });
     </script>
