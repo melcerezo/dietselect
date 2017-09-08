0c59d9ca30546b0cc10fe34992792f7cdaf50a51
melcerezo <melcerezo@gmail.com>
2017-09-08 23:32:40 +0800

    stat

--- resources/views/testMaterializeTemplates/testDashboard.blade.php
+++ resources/views/testMaterializeTemplates/testDashboard.blade.php
@@ -8,11 +8,11 @@
         $(document).ready(function () {
             $('div.buyCard').on('mouseenter',function () {
                 $(this).children('.buyBtn').show();
-    //            console.log('stuff');
+                console.log('stuff');
             });
             $('div.buyCard').on('mouseleave',function () {
                 $(this).children('.buyBtn').hide();
-    //            console.log('stuff');
+                console.log('stuff');
             });
         });
     </script>
