5ed5afbb79e18e27697657f5ea6064bd77025f4c
melcerezo <melcerezo@gmail.com>
2017-09-09 18:22:17 +0800

    plan

--- resources/views/testMaterializeTemplates/testMealPlanner.blade.php
+++ resources/views/testMaterializeTemplates/testMealPlanner.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.app')
 @section('head')
+    <style>
+        .picSection{ max-width:400px; max-height:400px; }
+    </style>
+    <script>
+
+    </script>
+@endsection
+@section('content')
     <div class="container">
         <div class="row">
             <span style="font-size: 30px;">Plan Name</span>
         </div>
         <div class="row">
             <div class="col s12 m6">
-                <div class="light-green">
+                <div class="light-green picSection">
                     <img src="/img/loss.png">
                 </div>
                 <div>
