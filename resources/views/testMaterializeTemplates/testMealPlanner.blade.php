2d92696fc5214becac0d331153d435617abd0f34
melcerezo <melcerezo@gmail.com>
2017-09-09 18:47:40 +0800

    plan

--- resources/views/testMaterializeTemplates/testMealPlanner.blade.php
+++ resources/views/testMaterializeTemplates/testMealPlanner.blade.php
@@ -2,6 +2,7 @@
 @section('head')
     <style>
         .picSection{ width:400px; height:400px; }
+        .galleryItem{ width:50px; height:50px; }
     </style>
     <script>
 
@@ -13,15 +14,35 @@
             <span style="font-size: 30px;">Plan Name</span>
         </div>
         <div class="row">
+            <div class="col s12 m2">
+                <div class="plan gallery">
+                    <div class="galleryItem">
+                        <img class="responsive-img" src="/img/loss.png">
+                    </div>
+                    <div class="galleryItem">
+                        <img class="responsive-img" src="/img/loss.png">
+                    </div>
+                    <div class="galleryItem">
+                        <img class="responsive-img" src="/img/loss.png">
+                    </div>
+                </div>
+            </div>
             <div class="col s12 m6">
                 <div class="light-green picSection">
                     <img class="responsive-img" src="/img/loss.png">
                 </div>
                 <div>
-
+                    <div class="chef">
+                    </div>
+                    <div class="section">
+                    </div>
+                    <div class="Description">
+                    </div>
+                    <div class="menu">
+                    </div>
                 </div>
             </div>
-            <div class="col s12 m6">
+            <div class="col s12 m4">
                 <div>
 
                 </div>
