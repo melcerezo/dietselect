a7b908480d877aa621da00e49ad2aff53dae2ae4
melcerezo <melcerezo@gmail.com>
2017-09-11 02:31:59 +0800

    single view

--- resources/views/foodie/viewSingleOrder.blade.php
+++ resources/views/foodie/viewSingleOrder.blade.php
@@ -3066,6 +3066,21 @@
                     @endif
                 @endif
                 </div>
+                <div class="row">
+                    @if($orderItem->order->is_paid==0 && $orderItem->order->is_cancelled==0)
+                        <a href="{{route('order.show',$orderItem->order->id)}}" class="btn waves-effect waves-light" style="font-weight: 100">Back to Order</a>
+                    @elseif($orderItem->order->is_paid==1 && $orderItem->order->is_cancelled==0)
+                        <a href="{{route('foodie.order.view', ['from'=>2])}}" class="btn waves-effect waves-light" style="font-weight: 100">Order History</a>
+                    @elseif($orderItem->order->is_cancelled==1)
+                        <a href="{{route('foodie.order.view', ['from'=>0])}}" class="btn waves-effect waves-light" style="font-weight: 100">Order History</a>
+                    @endif
+                    @if($orderItem->order_type==2)
+                        @if($orderPlan->simple_custom_plan_detail->count())
+                            <button data-target="planCustom" class="btn waves-effect waves-light modal-trigger" style="margin-left: 10px;">See Customizations</button>
+                        @endif
+                    @endif
+
+                </div>
             </div>
 
             @if($orderItem->order_type==2)
@@ -3230,22 +3245,7 @@
                     @endforeach
                 @endif
             @endunless
-            <div class="row">
-                @if($orderItem->order->is_paid==0 && $orderItem->order->is_cancelled==0)
-                    <a href="{{route('order.show',$orderItem->order->id)}}" class="btn waves-effect waves-light" style="font-weight: 100">Back to Order</a>
-                @elseif($orderItem->order->is_paid==1 && $orderItem->order->is_cancelled==0)
-                    <a href="{{route('foodie.order.view', ['from'=>2])}}" class="btn waves-effect waves-light" style="font-weight: 100">Order History</a>
-                @elseif($orderItem->order->is_cancelled==1)
-                    <a href="{{route('foodie.order.view', ['from'=>0])}}" class="btn waves-effect waves-light" style="font-weight: 100">Order History</a>
-                @endif
-                @if($orderItem->order_type==2)
-                    @if($orderPlan->simple_custom_plan_detail->count())
-                        <button data-target="planCustom" class="btn waves-effect waves-light modal-trigger" style="margin-left: 10px;">See Customizations</button>
-                    @endif
-                @endif
 
-            </div>
-        </div>
 
 
         {{--@unless($mealPlansCount==0)--}}
