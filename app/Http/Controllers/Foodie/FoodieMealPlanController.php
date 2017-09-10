6794c9c18d642f3fa0a53700a465c79ba0018a6a
melcerezo <melcerezo@gmail.com>
2017-09-11 02:06:21 +0800

    single view

--- app/Http/Controllers/Foodie/FoodieMealPlanController.php
+++ app/Http/Controllers/Foodie/FoodieMealPlanController.php
@@ -228,7 +228,7 @@ class FoodieMealPlanController extends Controller
             ->orWhere('detail','=','grilled')
             ->get();
 
-        dd($simpleCustomPlan);
+        dd($simpleCustomPlan->simple_custom_plan_detail()->get());
         $driedCount = $simpleCustomPlan->simple_custom_plan_detail()
             ->where('detail','=','preservatives')
             ->orWhere('detail','=','salt')
