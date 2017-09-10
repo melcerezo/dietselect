3a8e9d4c38f7cedddde5267b986a60d815e5b0b2
melcerezo <melcerezo@gmail.com>
2017-09-10 13:14:21 +0800

    master

--- resources/views/foodie/cart/index.blade.php
+++ resources/views/foodie/cart/index.blade.php
@@ -32,10 +32,10 @@
                                 <tr>
                                     <td>
                                         <div class="row">
-                                            <div class="col s12 m4">
+                                            <div class="col s12 m5">
                                                 <img class="responsive-img" src="/img/{{$cartItem->options->pic}}" alt="">
                                             </div>
-                                            <div class="col s12 m8">
+                                            <div class="col s12 m7">
                                                 <div style="font-size: 30px;">
                                                     {{$cartItem->name}}
                                                 </div>
