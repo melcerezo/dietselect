790aa25034d777eb0fd5d35927c8b0feb0c3ef55
melcerezo <melcerezo@gmail.com>
2017-09-11 11:16:46 +0800

    single view

--- resources/views/foodie/chefRating.blade.php
+++ resources/views/foodie/chefRating.blade.php
@@ -40,26 +40,26 @@
                                     {{csrf_field()}}
 
                                     <input type="hidden" name="rating" id="rate{{$key}}" data-error=".error{{$key}}" value=""/>
-                                    <div id="rateYo{{$key}}"></div>
-                                    <div class="error{{$key}} err">
-                                    </div>
-                                    <script>
-                                        $(function () {
-                                            $("#rateYo{{$key}}").rateYo({
-                                                rating: 1,
-                                                fullStar: true
-                                            });
+                                    {{--<div id="rateYo{{$key}}"></div>--}}
+                                    {{--<div class="error{{$key}} err">--}}
+                                    {{--</div>--}}
+                                    {{--<script>--}}
+                                        {{--$(function () {--}}
+                                            {{--$("#rateYo{{$key}}").rateYo({--}}
+                                                {{--rating: 1,--}}
+                                                {{--fullStar: true--}}
+                                            {{--});--}}
 
-                                            $("#rateYo{{$key}}").rateYo()
-                                                    .on("rateyo.set", function (e, data) {
-                                                        var $rate = data.rating;
+                                            {{--$("#rateYo{{$key}}").rateYo()--}}
+                                                    {{--.on("rateyo.set", function (e, data) {--}}
+                                                        {{--var $rate = data.rating;--}}
 
-                                                        $('input#rate{{$key}}').val($rate);
+                                                        {{--$('input#rate{{$key}}').val($rate);--}}
                                                         {{--window.alert("Its " + $('input#rate{{$key}}').val() + " Yo!");--}}
-                                                    });
+                                                    {{--});--}}
 
-                                        });
-                                    </script>
+                                        {{--});--}}
+                                    {{--</script>--}}
 
                                     {{--<span class="starRating">--}}
                                         {{--<input name="rate{{$key}}" type="radio" id="test1{{$key}}" value="1" checked/>--}}
