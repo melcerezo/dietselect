bfda799ff1a8075a0ca84d6181849f059e4fc110
melcerezo <melcerezo@gmail.com>
2017-09-12 21:49:37 +0800

    meal-planner edit

--- resources/views/chef/showAllOrders.blade.php
+++ resources/views/chef/showAllOrders.blade.php
@@ -105,18 +105,7 @@
                                         <div class="col s12 m2">
                                             <a href="{{route('chef.order.single', $order['id'])}}" class="orange darken-2 btn btn-primary waves-effect waves-light" style="font-weight: 100;">Details</a>
                                         </div>
-                                        {{--<div class="col s12 m2">--}}
-                                        {{--<button data-target="cancelAllModal{{$order->id}}" class="btn btn-primary waves-effect waves-light red modal-trigger" style="font-weight: 100;">Cancel</button>--}}
-                                        {{--</div>--}}
                                     </div>
-                                    {{--<div id="cancelAllModal{{$order->id}}" class="modal">--}}
-                                    {{--<div class="modal-content">--}}
-                                    {{--<form action="{{route('foodie.orderAll.cancel', $order->id)}}" method="post">--}}
-                                    {{--{{ csrf_field() }}--}}
-                                    {{--<button class="btn waves-effect waves-light">Cancel Order?</button>--}}
-                                    {{--</form>--}}
-                                    {{--</div>--}}
-                                    {{--</div>--}}
                                 </div>
                             </div>
                         @elseif($order['is_paid']==1 && $order['is_cancelled']==0)
@@ -177,14 +166,6 @@
                                         {{--<button data-target="cancelAllModal{{$order->id}}" class="btn btn-primary waves-effect waves-light red modal-trigger" style="font-weight: 100;">Cancel</button>--}}
                                         {{--</div>--}}
                                     </div>
-                                    {{--<div id="cancelAllModal{{$order->id}}" class="modal">--}}
-                                    {{--<div class="modal-content">--}}
-                                    {{--<form action="{{route('foodie.orderAll.cancel', $order->id)}}" method="post">--}}
-                                    {{--{{ csrf_field() }}--}}
-                                    {{--<button class="btn waves-effect waves-light">Cancel Order?</button>--}}
-                                    {{--</form>--}}
-                                    {{--</div>--}}
-                                    {{--</div>--}}
                                 </div>
                             </div>
                         @endif
@@ -246,18 +227,7 @@
                                         <div class="col s12 m2">
                                             <a href="{{route('chef.order.single', $order['id'])}}" class="orange darken-2 btn btn-primary waves-effect waves-light" style="font-weight: 100;">Details</a>
                                         </div>
-                                        {{--<div class="col s12 m2">--}}
-                                        {{--<button data-target="cancelAllModal{{$order->id}}" class="btn btn-primary waves-effect waves-light red modal-trigger" style="font-weight: 100;">Cancel</button>--}}
-                                        {{--</div>--}}
                                     </div>
-                                    {{--<div id="cancelAllModal{{$order->id}}" class="modal">--}}
-                                    {{--<div class="modal-content">--}}
-                                    {{--<form action="{{route('foodie.orderAll.cancel', $order->id)}}" method="post">--}}
-                                    {{--{{ csrf_field() }}--}}
-                                    {{--<button class="btn waves-effect waves-light">Cancel Order?</button>--}}
-                                    {{--</form>--}}
-                                    {{--</div>--}}
-                                    {{--</div>--}}
                                 </div>
                             </div>
                         @endif
@@ -323,14 +293,6 @@
                                         {{--<button data-target="cancelAllModal{{$order->id}}" class="btn btn-primary waves-effect waves-light red modal-trigger" style="font-weight: 100;">Cancel</button>--}}
                                         {{--</div>--}}
                                     </div>
-                                    {{--<div id="cancelAllModal{{$order->id}}" class="modal">--}}
-                                    {{--<div class="modal-content">--}}
-                                    {{--<form action="{{route('foodie.orderAll.cancel', $order->id)}}" method="post">--}}
-                                    {{--{{ csrf_field() }}--}}
-                                    {{--<button class="btn waves-effect waves-light">Cancel Order?</button>--}}
-                                    {{--</form>--}}
-                                    {{--</div>--}}
-                                    {{--</div>--}}
                                 </div>
                             </div>
                         @endif
@@ -392,18 +354,7 @@
                                         <div class="col s12 m2">
                                             <a href="{{route('chef.order.single', $order['id'])}}" class="orange darken-2 btn btn-primary waves-effect waves-light" style="font-weight: 100;">Details</a>
                                         </div>
-                                        {{--<div class="col s12 m2">--}}
-                                        {{--<button data-target="cancelAllModal{{$order->id}}" class="btn btn-primary waves-effect waves-light red modal-trigger" style="font-weight: 100;">Cancel</button>--}}
-                                        {{--</div>--}}
                                     </div>
-                                    {{--<div id="cancelAllModal{{$order->id}}" class="modal">--}}
-                                    {{--<div class="modal-content">--}}
-                                    {{--<form action="{{route('foodie.orderAll.cancel', $order->id)}}" method="post">--}}
-                                    {{--{{ csrf_field() }}--}}
-                                    {{--<button class="btn waves-effect waves-light">Cancel Order?</button>--}}
-                                    {{--</form>--}}
-                                    {{--</div>--}}
-                                    {{--</div>--}}
                                 </div>
                             </div>
                          </div>
@@ -411,77 +362,8 @@
                     @endif
                 @endforeach
             </div>
+
         </div>
-            {{--@foreach($orders as $order)--}}
-                {{--@if($order['is_cancelled']==0)--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="card">--}}
-                            {{--<div class="card-panel">--}}
-                                {{--<a href="{{route('chef.order.single', $order['id'])}}">--}}
-                                    {{--<table class="responsive-table centered" style="table-layout: fixed;">--}}
-                                        {{--<thead>--}}
-                                            {{--<tr>--}}
-                                                {{--<th>ID</th>--}}
-                                                {{--<th>Plan Name</th>--}}
-                                                {{--<th>Foodie Name</th>--}}
-                                                {{--<th>Quantity</th>--}}
-                                                {{--<th>Amount</th>--}}
-                                                {{--<th>Type</th>--}}
-                                                {{--<th>Payment Status</th>--}}
-                                            {{--</tr>--}}
-                                        {{--</thead>--}}
-                                        {{--<tbody>--}}
-                                            {{--<tr>--}}
-                                                {{--<td class="orderID">{{$order['id']}}</td>--}}
-                                                {{--<td class="planName">{{$order['plan_name']}}</td>--}}
-                                                {{--@foreach($foodies as $foodie)--}}
-                                                    {{--@if($foodie->id==$order['foodie_id'])--}}
-                                                        {{--<td class="foodieName">{{$foodie->first_name.' '.$foodie->last_name}}</td>--}}
-                                                    {{--@endif--}}
-                                                {{--@endforeach--}}
-                                                {{--<td class="quantity">{{$order['quantity']}}</td>--}}
-                                                {{--<td class="amount">{{$order['price']}}</td>--}}
-                                                {{--<td class="type">--}}
-                                                    {{--<p>--}}
-                                                        {{--@if($order['order_type']==0)--}}
-                                                            {{--<span>Standard</span>--}}
-                                                        {{--@elseif($order['order_type']==1 || $order['order_type']==2)--}}
-                                                            {{--<span>Customized</span>--}}
-                                                        {{--@endif--}}
-                                                    {{--</p>--}}
-                                                {{--</td>--}}
-                                                {{--<td class="paid">--}}
-                                                    {{--@if($order['is_cancelled']==0)--}}
-                                                        {{--@if($order['is_paid']==1)--}}
-                                                            {{--<p>Paid</p>--}}
-                                                        {{--@elseif($order['is_paid']==0)--}}
-                                                            {{--<p>Pending</p>--}}
-                                                        {{--@endif--}}
-                                                    {{--@else--}}
-                                                        {{--<p>Cancelled</p>--}}
-                                                    {{--@endif--}}
-                                                {{--</td>--}}
-                                                {{--<td>--}}
-                                                    {{--@if($order->is_paid==0)--}}
-                                                        {{--<p>Pending</p>--}}
-                                                    {{--@else--}}
-                                                        {{--<p>Paid</p>--}}
-                                                    {{--@endif--}}
-                                                {{--</td>--}}
-                                            {{--</tr>--}}
-                                        {{--</tbody>--}}
-                                    {{--</table>--}}
-                                {{--</a>--}}
-                            {{--@if($order['is_paid']==1)--}}
-                            {{--<div>--}}
-                                {{--<button onclick="window.location.href='{{route('chef.order.single',['order'=>$order['id']])}}'" class="btn">See Plan</button>--}}
-                            {{--</div>--}}
-                            {{--@endif--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--@endif--}}
-            {{--@endforeach--}}
         </div>
     @else
         No Orders Yet!
