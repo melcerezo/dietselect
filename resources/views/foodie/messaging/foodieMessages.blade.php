d79cdb1d2779882c2c12d8711932d97e7a9ec1ce
melcerezo <melcerezo@gmail.com>
2017-09-12 12:29:19 +0800

    plan

--- resources/views/foodie/messaging/foodieMessages.blade.php
+++ resources/views/foodie/messaging/foodieMessages.blade.php
@@ -73,7 +73,7 @@
                                 @if($chats->count() > 0)
                                     @foreach($chats as $chat)
                                         @if($chat->message->where('is_read',0)->where('receiver_type','f')->count()==0)
-                                            <li id="chtItem-{{$chat->id}}" class="collection-item msgItem">
+                                            <li id="chtItem-{{$chat->id}}" class="collection-item msgItem" style="border-radius: 0;">
                                                 <a href="{{route('foodie.message.message', $chat->id)}}">
                                                     <div>
                                                         @foreach($chefs as $chef)
