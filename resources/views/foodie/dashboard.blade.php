842309eadcb91ff202ba05d51590fba3b9cd4d94
melcerezo <melcerezo@gmail.com>
2017-09-12 23:28:55 +0800

    meal-planner edit

--- resources/views/foodie/dashboard.blade.php
+++ resources/views/foodie/dashboard.blade.php
@@ -341,7 +341,7 @@
                     <div class="file-field input-field">
                         <label for="planPic" class="active">Picture Upload:</label>
                         <div style="padding-top: 10px;">
-                            <div class="btn">
+                            <div class="btn orange darken-2">
                                 <span>File</span>
                                 <input type="file" data-error=".error-pic" id="cover" name="cover">
                             </div>
@@ -353,7 +353,7 @@
                     </div>
                 </div>
                 <div>
-                    <input type="submit" value="Submit" class="btn btn-primary">
+                    <input type="submit" value="Submit" class="btn orange darken-2 btn-primary">
                 </div>
             </form>
         </div>
