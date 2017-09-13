d7e280557175650efb8477730a0f806fed2a0e53
melcerezo <melcerezo@gmail.com>
2017-09-13 22:50:40 +0800

    stuff

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -27,9 +27,9 @@ class CartController extends Controller
     public function index()
     {
         $dt=Carbon::now();
-        $startOfNextWeek = $dt->startOfWeek()->addDay(7)->format('f d, Y');
+        $startOfNextWeek = $dt->startOfWeek()->addDay(7)->format('F d, Y');
         $ds=Carbon::now();
-        $endOfNextWeek = $ds->startOfWeek()->addDay(7)->addDay(4)->format('f d, Y');
+        $endOfNextWeek = $ds->startOfWeek()->addDay(7)->addDay(4)->format('F d, Y');
 
 
         $foodie= Auth::guard('foodie')->user()->id;
