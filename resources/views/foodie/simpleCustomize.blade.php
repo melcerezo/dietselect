f28b323bcc0c8738c4024aef844d437aef72373a
melcerezo <melcerezo@gmail.com>
2017-09-13 22:36:10 +0800

    stuff

--- resources/views/foodie/simpleCustomize.blade.php
+++ resources/views/foodie/simpleCustomize.blade.php
@@ -765,7 +765,7 @@
            <div class="modal-content">
                @if($tasteCount>0)
                    <ul class="collection">
-                       <li class="collection-item">
+                       <li class="collection-item" style="font-size: 20px;">
                            <span class="collection-header">Taste</span>
                        </li>
                        @foreach($simpleCustomPlan->simple_custom_plan_detail as $detail)
@@ -780,7 +780,7 @@
                @endif
                @if($cookCount>0)
                    <ul class="collection">
-                       <li class="collection-item">
+                       <li class="collection-item" style="font-size: 20px;">
                            <span class="collection-header">Cooking Methods</span>
                        </li>
                        @foreach($simpleCustomPlan->simple_custom_plan_detail as $detail)
@@ -794,7 +794,7 @@
                @endif
                @if($driedCount>0)
                    <ul class="collection">
-                       <li class="collection-item">
+                       <li class="collection-item" style="font-size: 20px;">
                            <span class="collection-header">Dry Goods/Condiments</span>
                        </li>
                        @foreach($simpleCustomPlan->simple_custom_plan_detail as $detail)
