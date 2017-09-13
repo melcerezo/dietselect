c5e9243357a02c599f0a801b41833524052adaea
melcerezo <melcerezo@gmail.com>
2017-09-13 22:43:53 +0800

    stuff

--- resources/views/foodie/cart/index.blade.php
+++ resources/views/foodie/cart/index.blade.php
@@ -112,7 +112,7 @@
                             </div>
                         </div>
                         <div style="margin: 10px 0;">
-                            <a id="orderButton" style="width: 100%;" href="{{route('foodie.order')}}" class="btn waves-effect waves-light orange darken-2">Order</a>
+                            <a id="orderButton" href="{{route('foodie.order')}}" class="btn waves-effect waves-light orange darken-2">Order</a>
                         </div>
                     </div>
                     {{--<div class="col s12 m2">--}}
