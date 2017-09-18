2e46d85983ed53f266f6a4c20992c33558b0b4a5
melcerezo <melcerezo@gmail.com>
2017-09-18 22:50:44 +0800

    profile

--- resources/views/chef/showSingleOrder.blade.php
+++ resources/views/chef/showSingleOrder.blade.php
@@ -22,7 +22,7 @@
                     <a href="{{route('chef.order.view', ['from'=>3])}}" class="orange-text text-darken-2">< Order History</a>
                 @endif
             </div>
-            <div class="orange-text text-darken-2" style="border-left: 4px solid #f57c00; margin: 10px 0;">
+            <div style="margin: 10px 0;">
                 <span style="font-size: 30px;">{{$planName}}</span>
                 <span style="font-size: 30px;"> - </span>
                 <span style="font-size: 30px;">
