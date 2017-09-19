a9e901ad0ac32af30c32d6f5b8917ece0503a20e
melcerezo <melcerezo@gmail.com>
2017-09-20 01:56:16 +0800

    foodie profile

--- public/js/foodie/foodieDash.js
+++ public/js/foodie/foodieDash.js
@@ -6,4 +6,29 @@ $(document).ready(function () {
            $(this).find('.deleteSimpleLink').css('visibility','hidden');
        }
    });
+
+    $('form#coverPhoto').validate({
+        rules: {
+            cover:{
+                required:true,
+                extension:true
+            }
+        },
+        messages: {
+            cover:{
+                required: 'Please choose a cover photo!',
+                extension: 'Images Only!'
+            }
+        },
+        errorElement : 'div',
+        errorPlacement: function(error, element) {
+            var placement = $(element).data('error');
+            if (placement) {
+                $(placement).append(error);
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+
 });
