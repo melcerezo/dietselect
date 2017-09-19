c35ce6b4ab1d46597f9a067ba34bd954870af04f
melcerezo <melcerezo@gmail.com>
2017-09-20 07:52:00 +0800

    foodie profile

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -119,7 +119,7 @@
                                             @endforeach
                                         </td>
                                         <td>{{$commission->created_at->format('F d, Y')}}</td>
-                                        <td>{{'PHP'.number_format($commission->amount,2,'.','')}}</td>
+                                        <td>{{'PHP '.number_format($commission->amount,2,'.',',')}}</td>
                                     </tr>
                                 @endforeach
                                 </tbody>
@@ -173,7 +173,7 @@
                                                 @endforeach
                                             </td>
                                             <td>{{$commission->created_at->format('F d, Y')}}</td>
-                                            <td>{{'PHP'.number_format($commission->amount,2,'.','')}}</td>
+                                            <td>{{'PHP '.number_format($commission->amount,2,'.',',')}}</td>
                                         </tr>
                                     @endforeach
                                     </tbody>
@@ -307,7 +307,7 @@
                                     <tr>
                                         <td>{{$plan->id}}</td>
                                         <td>{{$plan->plan_name}}</td>
-                                        <td>{{'PHP'.number_format($plan->price, 2, '.', '')}}</td>
+                                        <td>{{'PHP '.number_format($plan->price, 2, '.', ',')}}</td>
                                     </tr>
                                 @endforeach
                                 </tbody>
@@ -347,7 +347,7 @@
                                     <tr>
                                         <td>{{$order->id}}</td>
                                         <td>{{$order->created_at->format('F d, Y')}}</td>
-                                        <td>{{'PHP'.number_format($order->total, 2, '.', '')}}</td>
+                                        <td>{{'PHP '.number_format($order->total, 2, '.', ',')}}</td>
                                         <td>
                                             @if($order->is_cancelled==0)
                                                 @if($order->is_paid==0)
