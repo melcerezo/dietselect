b1e19ff4ed4baa1da06162276a441232fe68509a
melcerezo <melcerezo@gmail.com>
2017-09-19 22:43:06 +0800

    meal plan

--- resources/views/foodie/profile.blade.php
+++ resources/views/foodie/profile.blade.php
@@ -619,7 +619,7 @@
                         </div>
                         <div class="modal-footer">
                             {{--<input type="submit" class="hidden"/>--}}
-                            <a href="javascript:void(0)" class="prfSvBtn modal-action right-aligned right"><i class="fa fa-save" aria-hidden="true"></i> </a>
+                            <button class="prfSvBtn modal-action right-aligned right"><i class="fa fa-save" aria-hidden="true"></i> </button>
                         </div>
                     </form> <!-- End of preferences form -->
                 </div>
