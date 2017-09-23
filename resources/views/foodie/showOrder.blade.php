92f85945165d9488fd4ab52ab9c3d2d60e96f0c9
melcerezo <melcerezo@gmail.com>
2017-09-23 22:38:29 +0800

    order plan

--- resources/views/foodie/showOrder.blade.php
+++ resources/views/foodie/showOrder.blade.php
@@ -93,20 +93,20 @@
                                        Please login to your paypal account to pay.
                                    </div>
 
-                                    @if ($message = Session::get('success'))
-                                        <div class="custom-alerts alert alert-success fade in">
-                                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
-                                            {!! $message !!}
-                                        </div>
-                                        <?php Session::forget('success');?>
-                                    @endif
-                                    @if ($message = Session::get('error'))
-                                        <div class="custom-alerts alert alert-danger fade in">
-                                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
-                                            {!! $message !!}
-                                        </div>
-                                        <?php Session::forget('error');?>
-                                    @endif
+                                    {{--@if ($message = Session::get('success'))--}}
+                                        {{--<div class="custom-alerts alert alert-success fade in">--}}
+                                            {{--<button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>--}}
+                                            {{--{!! $message !!}--}}
+                                        {{--</div>--}}
+                                        {{--<?php Session::forget('success');?>--}}
+                                    {{--@endif--}}
+                                    {{--@if ($message = Session::get('error'))--}}
+                                        {{--<div class="custom-alerts alert alert-danger fade in">--}}
+                                            {{--<button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>--}}
+                                            {{--{!! $message !!}--}}
+                                        {{--</div>--}}
+                                        {{--<?php Session::forget('error');?>--}}
+                                    {{--@endif--}}
                                     <div>
                                         <form method="POST" id="payment-form" role="form" action="{{route('addmoney.paypal', compact('order'))}}" >
                                             {{ csrf_field() }}
