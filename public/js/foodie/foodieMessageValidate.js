acfb9a2af269502c952ecefd9ccc2e7ffbcffc9d
melcerezo <melcerezo@gmail.com>
2017-09-26 03:43:56 +0800

    message

--- public/js/foodie/foodieMessageValidate.js
+++ public/js/foodie/foodieMessageValidate.js
@@ -32,8 +32,8 @@ $(document).ready(function () {
 
     $('#createSubmit').click(function () {
         var msgVal = $('#foodieMessage').val();
-        console.log(msgVal);
-        // console.log($.trim('msgVal').length);
+        // console.log(msgVal);
+        console.log($.trim('msgVal'));
 
         // if($.trim('msgVal').length==0){
         //     $('div.error-message').empty();
