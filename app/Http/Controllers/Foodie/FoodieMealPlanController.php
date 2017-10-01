ae87a38e93f5b85c48fc612f09277fbcd9edd718
melcerezo <melcerezo@gmail.com>
2017-10-01 16:26:33 +0800

    spaces for allergies

--- app/Http/Controllers/Foodie/FoodieMealPlanController.php
+++ app/Http/Controllers/Foodie/FoodieMealPlanController.php
@@ -112,7 +112,7 @@ class FoodieMealPlanController extends Controller
             }
         } // END OF DEADLINE SATURDAY @ 3 PM
 
-        dd($foodiePreference);
+//        dd($foodiePreference);
 //        dd($this->smsIsUnverified());
 
         $notifications=Notification::where('receiver_id','=',$foodie)->where('receiver_type','=','f')->get();
