4bdc052c873820143e6e124c5a47bf9215559f35
melcerezo <melcerezo@gmail.com>
2017-10-01 16:22:57 +0800

    spaces for allergies

--- app/Http/Controllers/Foodie/FoodieMealPlanController.php
+++ app/Http/Controllers/Foodie/FoodieMealPlanController.php
@@ -31,6 +31,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 use phpDocumentor\Reflection\Types\Integer;
 
 
