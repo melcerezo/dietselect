87ddc59d0db68e05ccaa6ad64da99cfc8a45b33a
melcerezo <melcerezo@gmail.com>
2017-10-04 05:31:52 +0800

    order all

--- public/js/foodie/orderAll.js
+++ public/js/foodie/orderAll.js
@@ -76,6 +76,7 @@ $(document).ready(function () {
 
         var dateChange = dateChoose(val);
         dateChange.done(function (response) {
+            $('div#ordAllContent').hide();
             $('div#dayPick').empty();
             var valData = JSON.parse(response);
             // console.log(JSON.parse(response));
