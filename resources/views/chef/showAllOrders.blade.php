5c8946790078fd6142cd4d5575ad61f7bcbd1330
melcerezo <melcerezo@gmail.com>
2017-10-05 20:18:36 +0800

    order controller

--- resources/views/chef/showAllOrders.blade.php
+++ resources/views/chef/showAllOrders.blade.php
@@ -50,6 +50,7 @@
                     </ul>
                     <div>
                         <ul class="collection">
+                            <li class="collection-item">Total Orders: {{$orderItems->count()}}</li>
                             <li class="collection-item"><div>Total Paid:</div> <span style="font-size: 14px;">PHP {{number_format($totalPaid,2,'.',',')}}</span></li>
                             <li class="collection-item"><div>Total Unpaid:</div> <span style="font-size: 14px;">PHP {{number_format($pendPaid,2,'.',',')}}</span></li>
                             {{--<li class="collection-item"></li>--}}
