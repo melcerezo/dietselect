c2eb14e69b8bf6df17bb18486b789903954ece28
melcerezo <melcerezo@gmail.com>
2017-10-05 20:26:40 +0800

    orders all

--- resources/views/chef/showAllOrders.blade.php
+++ resources/views/chef/showAllOrders.blade.php
@@ -51,7 +51,8 @@
                     <div>
                         <ul class="collection">
                             <li class="collection-item"><div>Total Orders:</div> <span style="font-size: 14px;">{{$orderItems->count()}}</span></li>
-                            <li class="collection-item"><div>Total Delivered Orders:</div> <span style="font-size: 14px;">{{$orderItems->where('is_delivered','=',1)->count()}}</span></li>
+                            <li class="collection-item"><div>Pending Delivery:</div> <span style="font-size: 14px;">{{$orderItems->where('is_delivered','=',1)->count()}}</span></li>
+                            <li class="collection-item"><div>Delivered Orders:</div> <span style="font-size: 14px;">{{$orderItems->where('is_delivered','=',1)->count()}}</span></li>
                             <li class="collection-item"><div>Total Paid:</div> <span style="font-size: 14px;">PHP {{number_format($totalPaid,2,'.',',')}}</span></li>
                             <li class="collection-item"><div>Total Unpaid:</div> <span style="font-size: 14px;">PHP {{number_format($pendPaid,2,'.',',')}}</span></li>
                             {{--<li class="collection-item"></li>--}}
