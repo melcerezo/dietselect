a73c1fbee41819c58066c124d96adb3d9bfc9e3e
melcerezo <melcerezo@gmail.com>
2017-10-05 16:17:11 +0800

    order plan controller

--- app/Http/Controllers/Foodie/FoodieOrderPlanController.php
+++ app/Http/Controllers/Foodie/FoodieOrderPlanController.php
@@ -425,7 +425,7 @@ class FoodieOrderPlanController extends Controller
             }
 //        }
 
-        dd($pendId);
+        dd('no');
 
         $notifications = Notification::where('receiver_id', '=', $foodie->id)->where('receiver_type', '=', 'f')->get();
         $unreadNotifications = Notification::where('receiver_id', '=', $foodie->id)->where('receiver_type', '=', 'f')->where('is_read', '=', 0)->count();
