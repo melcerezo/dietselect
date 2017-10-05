e96559c9e9228f9f67783376113e726185d8815d
melcerezo <melcerezo@gmail.com>
2017-10-05 15:55:58 +0800

    order plan controller

--- app/Http/Controllers/Foodie/FoodieOrderPlanController.php
+++ app/Http/Controllers/Foodie/FoodieOrderPlanController.php
@@ -397,7 +397,8 @@ class FoodieOrderPlanController extends Controller
         //pending orders
         $pendingOrders = Order::where('is_paid', '=', 0)->where('is_cancelled', '=', 0)->where('foodie_id', '=', $foodie->id)->where('created_at', '>', $lastSaturday)->latest()->get();
 
-        if($pendingOrders>0){
+        dd($pendingOrders);
+        if($pendingOrders->count > 0){
             $notfound = 0;
             foreach ($pendingOrders as $pendingOrder) {
                 $orderItems = $pendingOrder->order_item()->get();
