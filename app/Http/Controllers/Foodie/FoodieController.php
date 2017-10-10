0e3e14ca13019329eb53bb1beb811afbb213c2d6
melcerezo <melcerezo@gmail.com>
2017-10-10 17:13:29 +0800

    order chef

--- app/Http/Controllers/Foodie/FoodieController.php
+++ app/Http/Controllers/Foodie/FoodieController.php
@@ -321,7 +321,7 @@ class FoodieController extends Controller
                                 $type="Customized";
                             } elseif ($orderItem->order_type == 2) {
                                 $orderPlan = CustomPlan::where('id', '=', $orderItem->plan_id)->first();
-                                dd($orderPlan);
+                                dd($orderItem);
 //                                $planName = $orderPlan->plan->plan_name;
                                 $type="Customized";
                             }
