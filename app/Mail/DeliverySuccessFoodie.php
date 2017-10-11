75333dac1abef4900d357c1d40f426eb515b28e3
melcerezo <melcerezo@gmail.com>
2017-10-12 02:13:11 +0800

    order delivery

---
+++ app/Mail/DeliverySuccessFoodie.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class DeliverySuccessFoodie extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $chefName;
+
+    public $planName;
+
+    public $time;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($chefName,$planName,$time)
+    {
+        $this->chefName = $chefName;
+        $this->planName = $planName;
+        $this->time = $time;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->from('diet@dietselect.com')
+            ->view('email.deliverFoodie');
+    }
+}
