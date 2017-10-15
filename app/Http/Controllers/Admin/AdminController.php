96f2e18068ca54a4f569d45ffdd730b158c6afae
melcerezo <melcerezo@gmail.com>
2017-10-15 11:45:32 +0800

    commissions select

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -552,7 +552,7 @@ class AdminController extends Controller
         $thisInput=null;
 
         if($type==0){
-            $commissions = Commission::groupBy('chef_id')->orderBy('created_at', 'desc')->get();
+            $commissions = Commission::orderBy('created_at', 'desc')->get();
             $i=0;
             if($commissions->count()){
                 $thisInput ='[';
