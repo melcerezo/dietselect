265ddaad748795476b3d232b337ab180ba8713a7
melcerezo <melcerezo@gmail.com>
2017-10-24 13:14:12 +0800

    show single order cancel

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -85,9 +85,10 @@ class AdminController extends Controller
         foreach($months as $month){
             foreach($commissions->where('created_at','>',$month['start'])->where('created_at','<',$month['end']) as $commission){
                 $month['coms']=$commission;
-//                dd($month);
+                dd($month);
             }
         }
+
         dd($months);
 
 
