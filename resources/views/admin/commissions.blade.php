0da5e23391c40dde6434988076a29ca33890fc7c
melcerezo <melcerezo@gmail.com>
2017-10-26 09:03:34 +0800

    commissions picker

--- resources/views/admin/commissions.blade.php
+++ resources/views/admin/commissions.blade.php
@@ -637,29 +637,32 @@
                                                     });
                                                 });
 
-                                                var valType = $('select#typeFilter{{$uniqueComChef}}').val();
 
-                                                if(valType==0){
-                                                    $('#monthPicker{{$uniqueComChef}}').show();
-                                                    $('#pendMonthPicker{{$uniqueComChef}}').hide();
-                                                    $('#paidMonthPicker{{$uniqueComChef}}').hide();
-                                                    $('#cancelMonthPicker{{$uniqueComChef}}').hide();
-                                                }else if(valType==1){
-                                                    $('#monthPicker{{$uniqueComChef}}').hide();
-                                                    $('#pendMonthPicker{{$uniqueComChef}}').show();
-                                                    $('#paidMonthPicker{{$uniqueComChef}}').hide();
-                                                    $('#cancelMonthPicker{{$uniqueComChef}}').hide();
-                                                }else if(valType==2){
-                                                    $('#monthPicker{{$uniqueComChef}}').hide();
-                                                    $('#pendMonthPicker{{$uniqueComChef}}').hide();
-                                                    $('#paidMonthPicker{{$uniqueComChef}}').show();
-                                                    $('#cancelMonthPicker{{$uniqueComChef}}').hide();
-                                                }else if(valType==3){
-                                                    $('#monthPicker{{$uniqueComChef}}').hide();
-                                                    $('#pendMonthPicker{{$uniqueComChef}}').hide();
-                                                    $('#paidMonthPicker{{$uniqueComChef}}').hide();
-                                                    $('#cancelMonthPicker{{$uniqueComChef}}').show();
-                                                }
+                                                $('select#typeFilter').change(function(){
+                                                    var valType = $('select#typeFilter{{$uniqueComChef}}').val();
+
+                                                    if(valType==0){
+                                                        $('#monthPicker{{$uniqueComChef}}').show();
+                                                        $('#pendMonthPicker{{$uniqueComChef}}').hide();
+                                                        $('#paidMonthPicker{{$uniqueComChef}}').hide();
+                                                        $('#cancelMonthPicker{{$uniqueComChef}}').hide();
+                                                    }else if(valType==1){
+                                                        $('#monthPicker{{$uniqueComChef}}').hide();
+                                                        $('#pendMonthPicker{{$uniqueComChef}}').show();
+                                                        $('#paidMonthPicker{{$uniqueComChef}}').hide();
+                                                        $('#cancelMonthPicker{{$uniqueComChef}}').hide();
+                                                    }else if(valType==2){
+                                                        $('#monthPicker{{$uniqueComChef}}').hide();
+                                                        $('#pendMonthPicker{{$uniqueComChef}}').hide();
+                                                        $('#paidMonthPicker{{$uniqueComChef}}').show();
+                                                        $('#cancelMonthPicker{{$uniqueComChef}}').hide();
+                                                    }else if(valType==3){
+                                                        $('#monthPicker{{$uniqueComChef}}').hide();
+                                                        $('#pendMonthPicker{{$uniqueComChef}}').hide();
+                                                        $('#paidMonthPicker{{$uniqueComChef}}').hide();
+                                                        $('#cancelMonthPicker{{$uniqueComChef}}').show();
+                                                    }
+                                                });
 
                                                 $('select#monthFilter{{$uniqueComChef}}').change(function (){
                                                     var selectVal = $('select#monthFilter{{$uniqueComChef}}').val();
