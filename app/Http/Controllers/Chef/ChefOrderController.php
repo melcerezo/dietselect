a591822baccffe5f15b76c1496e2423e1c84e1cc
melcerezo <melcerezo@gmail.com>
2017-11-02 06:07:52 +0800

    chef commission mail

--- app/Http/Controllers/Chef/ChefOrderController.php
+++ app/Http/Controllers/Chef/ChefOrderController.php
@@ -47,9 +47,11 @@ class ChefOrderController extends Controller
 
         $chef = Auth::guard('chef')->user();
 
-        $orderItems=OrderItem::where('chef_id','=', $chef->id)->join('orders','order_items.order_id','=','orders.id')->orderBy('is_paid','ASC')
-            ->orderBy('order_items.created_at','desc')
-            ->select('*','order_items.id as it_id')->get();
+        $orderItems=OrderItem::where('chef_id','=', $chef->id)
+//            ->join('orders','order_items.order_id','=','orders.id')->orderBy('is_paid','ASC')
+//            ->orderBy('order_items.created_at','desc')
+//            ->select('*','order_items.id as it_id')
+            ->get();
 
         dd($orderItems);
 
