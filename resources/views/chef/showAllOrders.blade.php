870a1336fbafea2b6b7c6cb53aa2f1cd09bc9b92
melcerezo <melcerezo@gmail.com>
2017-11-02 07:23:00 +0800

    chef commission mail

--- resources/views/chef/showAllOrders.blade.php
+++ resources/views/chef/showAllOrders.blade.php
@@ -50,8 +50,8 @@
                     </ul>
                     <div>
                         <ul class="collection">
-                            <li class="collection-item"><div>Total Orders:</div> <span style="font-size: 14px;">{{$orderItems->where('order_items.is_cancelled','=',0)->count()}} orders</span></li>
-                            <li class="collection-item"><div>Pending Delivery:</div> <span style="font-size: 14px;">{{$orderItems->where('is_delivered','=',0)->where('order_items.is_cancelled','=',0)->count()}} orders</span></li>
+                            <li class="collection-item"><div>Total Orders:</div> <span style="font-size: 14px;">{{$orderItems->where('is_cancelled','=',0)->count()}} orders</span></li>
+                            <li class="collection-item"><div>Pending Delivery:</div> <span style="font-size: 14px;">{{$orderItems->where('is_delivered','=',0)->where('is_cancelled','=',0)->count()}} orders</span></li>
                             <li class="collection-item"><div>Delivered:</div> <span style="font-size: 14px;">{{$orderItems->where('is_delivered','=',1)->count()}} orders</span></li>
                             <li class="collection-item"><div>Total Paid:</div> <span style="font-size: 14px;">PHP {{number_format($totalPaid,2,'.',',')}}</span></li>
                             <li class="collection-item"><div>Total Unpaid:</div> <span style="font-size: 14px;">PHP {{number_format($pendPaid,2,'.',',')}}</span></li>
