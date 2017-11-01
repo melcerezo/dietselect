2bf3d42487fa65f1bb92983580a89fba9dca215c
melcerezo <melcerezo@gmail.com>
2017-11-02 07:42:37 +0800

    chef commission mail

--- app/Http/Controllers/Chef/ChefOrderController.php
+++ app/Http/Controllers/Chef/ChefOrderController.php
@@ -59,7 +59,7 @@ class ChefOrderController extends Controller
 
         $totalPaid=0;
 
-        foreach($orderItems->where('is_paid','=',1) as $orderItem){
+        foreach($orderItems->where('orders.is_paid','=',1) as $orderItem){
             $totalPaid+=$orderItem->price;
         }
 
